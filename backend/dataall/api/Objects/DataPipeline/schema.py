44a218d879d32d282187932d648232d24c4a4de9
dlpzx <dlpzx@amazon.com>
2022-06-20 14:39:58 +0200

    Added template in datapipeline graphql schema

--- backend/dataall/api/Objects/DataPipeline/schema.py
+++ backend/dataall/api/Objects/DataPipeline/schema.py
@@ -23,6 +23,7 @@ DataPipeline = gql.ObjectType(
         ),
         gql.Field('inputDatasetUri', type=gql.String),
         gql.Field('outputDatasetUri', type=gql.String),
+        gql.Field('template', type=gql.String),
         gql.Field('devStages', type=gql.ArrayType(gql.String)),
         gql.Field('devStrategy', type=gql.String),
         gql.Field('cloneUrlHttp', gql.String, resolver=get_clone_url_http),
