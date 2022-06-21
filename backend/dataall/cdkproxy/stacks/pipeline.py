9dee924ce0e877c2c7f6a1dfd91e946b9ca08f2f
dlpzx <dlpzx@amazon.com>
2022-06-21 08:22:41 +0200

    rollback change of directories for blueprint

--- backend/dataall/cdkproxy/stacks/pipeline.py
+++ backend/dataall/cdkproxy/stacks/pipeline.py
@@ -157,7 +157,7 @@ class PipelineStack(Stack):
         code_dir_path = os.path.realpath(
             os.path.abspath(
                 os.path.join(
-                    __file__, "..", "..", "data_pipeline_blueprint"
+                    __file__, "..", "..", "..", "..", "blueprints", "data_pipeline_blueprint"
                 )
             )
         )
