b492bc7e5c35a7689dc0d959e42af470c58affda
dlpzx <dlpzx@amazon.com>
2022-09-02 11:00:30 +0200

    Fix graphql api for pipeline environments - runs

--- backend/dataall/db/models/__init__.py
+++ backend/dataall/db/models/__init__.py
@@ -32,6 +32,7 @@ from .ShareObject import ShareObject
 from .ShareObjectHistory import ShareObjectHistory
 from .ShareObjectItem import ShareObjectItem
 from .DataPipeline import DataPipeline
+from .DataPipelineEnvironment import DataPipelineEnvironment
 from .Stack import Stack
 from .Tag import Tag, ItemTags, updateObjectTags
 from .Task import Task
