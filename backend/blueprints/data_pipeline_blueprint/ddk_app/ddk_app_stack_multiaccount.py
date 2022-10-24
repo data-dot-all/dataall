e107121adcef9988f9c2b532aaf65a4ce5b0e70a
dlpzx <dlpzx@amazon.com>
2022-10-24 11:18:42 +0200

    Modifications to tag resources in multi-account pipelines non-cdk

--- backend/blueprints/data_pipeline_blueprint/ddk_app/ddk_app_stack_multiaccount.py
+++ backend/blueprints/data_pipeline_blueprint/ddk_app/ddk_app_stack_multiaccount.py
@@ -1,6 +1,6 @@
 from typing import Any, Optional
 
-from aws_cdk import Environment
+from aws_cdk import Environment, Tags
 from aws_ddk_core.base import BaseStack
 from aws_ddk_core.config import Config
 from constructs import Construct
@@ -22,5 +22,7 @@ class DdkApplicationStack(BaseStack):
             environment_id=environment_id,
             env=env or self._config.get_env(environment_id),
             **kwargs)
+
+        Tags.of(self).add("Team", str(env_vars['Team']))
 
         # The code that defines your stack goes here:
