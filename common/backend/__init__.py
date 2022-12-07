2b4a52557fbf86894c6cdc6ba355f65c0aad8df9
dlpzx <dlpzx@amazon.com>
2022-12-07 16:26:53 +0100

    remove init from aws_handlers

--- common/backend/__init__.py
+++ common/backend/__init__.py
@@ -1,9 +1,8 @@
 print("Initializing common Python package...")
 from common.api import *
-from common.aws_handlers import *
 from common.db import *
-from common.search_proxy import *
 from common.utils import *
+from common.search_proxy import *
 from common import api_handler
 print("All packages and files have been successfully initialized")
 
