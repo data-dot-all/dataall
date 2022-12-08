79b820220f4b1c5fa1da8461a9d7e63f6904dcdf
dlpzx <dlpzx@amazon.com>
2022-12-08 11:48:12 +0100

    remove api handler from init

--- common/backend/__init__.py
+++ common/backend/__init__.py
@@ -2,6 +2,5 @@ print("Initializing common Python package...")
 from common.api import *
 from common.db import *
 from common.search_proxy import *
-from common import api_handler
 print("All packages and files have been successfully initialized")
 
