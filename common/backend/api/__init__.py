b97b885d45b804c70da0db1de65298103f5449dc
dlpzx <dlpzx@amazon.com>
2022-12-08 17:20:54 +0100

    back reimport

--- common/backend/api/__init__.py
+++ common/backend/api/__init__.py
@@ -4,10 +4,10 @@ from backend.api.context import (
     GraphQLEnumMapper,
     Context
 )
-#from backend.api.Objects import (
-#    bootstrap,
-#    get_executable_schema,
-#    resolver_adapter,
-#    save
-#)
+from backend.api.Objects import (
+    bootstrap,
+    get_executable_schema,
+    resolver_adapter,
+    save
+)
 print("api Python package successfully initialized")
