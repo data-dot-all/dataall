797d122d7ecbf375d616e3c1a1454bf7840e2d87
dlpzx <dlpzx@amazon.com>
2022-12-09 14:15:24 +0100

    renamed and moved db operations

--- common/backend/db/models/__init__.py
+++ common/backend/db/models/__init__.py
@@ -1,4 +1,6 @@
 from backend.db.models.Permission import Permission, PermissionType
+from backend.db.models.Stack import Stack
+from backend.db.models.Task import Task
 from backend.db.models.Tenant import Tenant
 from backend.db.models.TenantPolicy import TenantPolicy
 from backend.db.models.TenantPolicyPermission import TenantPolicyPermission
@@ -6,6 +8,8 @@ from backend.db.models.TenantPolicyPermission import TenantPolicyPermission
 __all__ = [
     "Permission",
     "PermissionType",
+    "Stack",
+    "Task",
     "Tenant",
     "TenantPolicy",
     "TenantPolicyPermission",
---
+++ common/backend/db/operations/__init__.py
@@ -0,0 +1,4 @@
+from .permission import Permission
+from .permission_checker import has_tenant_perm, has_resource_perm
+from .resource_policy import ResourcePolicy
+from .stack import Stack
