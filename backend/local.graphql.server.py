12d7683d4acb76e1df27f4d33dbc18b50ba630e2
dlpzx <dlpzx@amazon.com>
2022-12-21 09:34:14 +0100

    backgraphqllocal

--- backend/local.graphql.server.py
+++ backend/local.graphql.server.py
@@ -49,10 +49,8 @@ CORS(app)
 
 def request_context(headers, mock=False):
     if mock:
-        #username = headers.get('username', 'anonymous@amazon.com')
-        #groups = headers.get('groups', ['Scientists', 'DAAdministrators', 'Engineers', 'Other'])
-        username = headers.get('username', 'requester@amazon.com')
-        groups = headers.get('groups', ['Scientists', 'DAAdministrators', 'Other'])
+        username = headers.get('username', 'anonymous@amazon.com')
+        groups = headers.get('groups', ['Scientists', 'DAAdministrators', 'Engineers', 'Other'])
     else:
         if not headers.get('Authorization'):
             raise Exception('Missing Authorization header')
