853e411fbeb30ed902947fc75a7f0cdafdc0e0f1
Noah Paige <noahpaig@amazon.com>
2023-01-03 15:42:48 -0500

    Add LF Tag Permissions and fix LF Tag Creation for new envs

---
+++ backend/dataall/db/models/LFTagPermissions.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, String, DateTime, ARRAY
+
+from .. import Base
+from .. import Resource, utils
+import datetime
+from sqlalchemy.dialects import postgresql
+
+
+class LFTagPermissions(Base):
+    __tablename__ = 'lftagpermissions'
+    tagPermissionUri = Column(String, primary_key=True, default=utils.uuid('lftag'))
+    SamlGroupName = Column(String, nullable=False)
+    environmentUri = Column(String, nullable=False)
+    # environmentLabel = Column(String, nullable=False)
+    awsAccount = Column(String, nullable=False)
+    tagKey = Column(String, nullable=False)
+    tagValues = Column(ARRAY(String))
