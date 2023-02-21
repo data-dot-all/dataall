85d35887c03dd8db6f847951e239b9851e778098
Nikita Podshivalov <nikpodsh@amazon.com>
2023-02-21 11:40:49 +0100

    Refactoring of the notebooks
    Fixed missed permissions

--- backend/dataall/modules/notebooks/__init__.py
+++ backend/dataall/modules/notebooks/__init__.py
@@ -1,4 +1,4 @@
 """Contains the code related to SageMaker notebooks"""
-from dataall.modules.notebooks import gql, models, cdk, services
+from dataall.modules.notebooks import gql, models, cdk, services, permissions
 
-__all__ = ["gql", "models", "cdk", "services"]
+__all__ = ["gql", "models", "cdk", "services", "permissions"]
