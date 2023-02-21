ca261d09a5e8e237a7289a5965a943df18f5a0a2
Nikita Podshivalov <nikpodsh@amazon.com>
2023-02-21 16:10:38 +0100

    Refactoring of TargetType
    Changed the registration mechanism
    Added minimum documentation

--- backend/dataall/db/api/target_type.py
+++ backend/dataall/db/api/target_type.py
@@ -1,64 +1,43 @@
 import logging
 
-from .. import exceptions, permissions
-from .. import models
+from dataall.db import exceptions, permissions
 
 logger = logging.getLogger(__name__)
 
 
 class TargetType:
-    @staticmethod
-    def get_target_type_permissions():
-        return dict(
-            dataset=(permissions.GET_DATASET, permissions.UPDATE_DATASET),
-            environment=(permissions.GET_ENVIRONMENT, permissions.UPDATE_ENVIRONMENT),
-            notebook=(permissions.GET_NOTEBOOK, permissions.UPDATE_NOTEBOOK),
-            mlstudio=(
-                permissions.GET_SGMSTUDIO_NOTEBOOK,
-                permissions.UPDATE_SGMSTUDIO_NOTEBOOK,
-            ),
-            pipeline=(permissions.GET_PIPELINE, permissions.UPDATE_PIPELINE),
-            redshift=(
-                permissions.GET_REDSHIFT_CLUSTER,
-                permissions.GET_REDSHIFT_CLUSTER,
-            ),
-        )
+    """Resolves the read/write permissions for different type of resources (target types)"""
+    _TARGET_TYPES = {}
+
+    def __init__(self, name, read_permission, write_permission):
+        self.name = name
+        self.read_permission = read_permission
+        self.write_permission = write_permission
+
+        TargetType._TARGET_TYPES[name] = self
 
     @staticmethod
     def get_resource_update_permission_name(target_type):
         TargetType.is_supported_target_type(target_type)
-        return TargetType.get_target_type_permissions()[target_type][1]
+        return TargetType._TARGET_TYPES[target_type].write_permission
 
     @staticmethod
     def get_resource_read_permission_name(target_type):
         TargetType.is_supported_target_type(target_type)
-        return TargetType.get_target_type_permissions()[target_type][0]
+        return TargetType._TARGET_TYPES[target_type].read_permission
 
     @staticmethod
     def is_supported_target_type(target_type):
-        supported_types = [
-            'dataset',
-            'environment',
-            'notebook',
-            'mlstudio',
-            'pipeline',
-            'redshift',
-        ]
-        if target_type not in supported_types:
+        if target_type not in TargetType._TARGET_TYPES:
             raise exceptions.InvalidInput(
                 'targetType',
                 target_type,
-                ' or '.join(supported_types),
+                ' or '.join(TargetType._TARGET_TYPES.keys()),
             )
 
-    @staticmethod
-    def get_target_type(model_name):
-        target_types_map = dict(
-            environment=models.Environment,
-            dataset=models.Dataset,
-            notebook=models.SagemakerNotebook,
-            mlstudio=models.SagemakerStudioUserProfile,
-            pipeline=models.DataPipeline,
-            redshift=models.RedshiftCluster,
-        )
-        return [k for k, v in target_types_map.items() if v == model_name][0]
+
+TargetType("dataset", permissions.GET_DATASET, permissions.UPDATE_DATASET)
+TargetType("environment", permissions.GET_ENVIRONMENT, permissions.UPDATE_ENVIRONMENT)
+TargetType("mlstudio", permissions.GET_SGMSTUDIO_NOTEBOOK, permissions.UPDATE_SGMSTUDIO_NOTEBOOK)
+TargetType("pipeline", permissions.GET_PIPELINE, permissions.UPDATE_PIPELINE)
+TargetType("redshift", permissions.GET_REDSHIFT_CLUSTER,permissions.GET_REDSHIFT_CLUSTER)
--- backend/dataall/modules/notebooks/__init__.py
+++ backend/dataall/modules/notebooks/__init__.py
@@ -1,4 +1,7 @@
 """Contains the code related to SageMaker notebooks"""
+from dataall.db.api import TargetType
 from dataall.modules.notebooks import gql, models, cdk, services, permissions
 
 __all__ = ["gql", "models", "cdk", "services", "permissions"]
+
+TargetType("notebook", permissions.GET_NOTEBOOK, permissions.UPDATE_NOTEBOOK)
