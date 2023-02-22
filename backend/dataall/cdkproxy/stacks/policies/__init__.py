8bf66df361a21956ac018162554f096b14c05d81
Nikita Podshivalov <nikpodsh@amazon.com>
2023-02-22 12:15:20 +0100

    Fixing cycle dependencies
    Sagemaker ML studio should still be loaded directly. It causes cycle dependency

--- backend/dataall/cdkproxy/stacks/policies/__init__.py
+++ backend/dataall/cdkproxy/stacks/policies/__init__.py
@@ -5,5 +5,8 @@ from dataall.cdkproxy.stacks.policies import (
     lakeformation, quicksight, redshift, stepfunctions, data_policy, service_policy
 )
 
+#TODO : should go away after refactoring of ml studios
+import dataall.modules.common.sagemaker.policies as sagemaker
+
 __all__ = ["_lambda", "cloudformation", "codestar", "databrew", "glue", "lakeformation", "quicksight",
-           "redshift", "stepfunctions", "data_policy", "service_policy"]
+           "redshift", "stepfunctions", "data_policy", "service_policy", "sagemaker"]
--- backend/dataall/modules/common/sagemaker/__init__.py
+++ backend/dataall/modules/common/sagemaker/__init__.py
@@ -1,5 +1,1 @@
 """Common code for machine learning studio and notebooks"""
-
-from dataall.modules.common.sagemaker import permissions, policies
-
-__all__ = ["policies", "permissions"]
