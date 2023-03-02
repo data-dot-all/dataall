8b2a04cf6507c233524ee0c6ac498b1f551ff77a
Nikita Podshivalov <nikpodsh@amazon.com>
2023-03-02 13:38:02 +0100

    Modules should be loaded before creation of db schema

--- backend/local_graphql_server.py
+++ backend/local_graphql_server.py
@@ -30,13 +30,13 @@ engine = get_engine(envname=ENVNAME)
 es = connect(envname=ENVNAME)
 logger.info('Connected')
 # create_schema_and_tables(engine, envname=ENVNAME)
+load_modules(modes=[ImportMode.API, ImportMode.TASKS])
 Base.metadata.create_all(engine.engine)
 CDKPROXY_URL = (
     'http://cdkproxy:2805' if ENVNAME == 'dkrcompose' else 'http://localhost:2805'
 )
 config.set_property("cdk_proxy_url", CDKPROXY_URL)
 
-load_modules(modes=[ImportMode.API, ImportMode.TASKS])
 init_permissions(engine)
 
 
