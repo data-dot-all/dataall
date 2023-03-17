4f1387698071e9e337ca85d6ba5797e8f6fbf97d
dlpzx <dlpzx@amazon.com>
2023-03-17 11:55:10 +0100

    Typo in ECS policy for CICD update

--- deploy/stacks/container.py
+++ deploy/stacks/container.py
@@ -339,7 +339,7 @@ class ContainerStack(pyNestedClass):
                     f"arn:aws:kms:{self.region}:{self.account}:key/*",
                     f"arn:aws:ssm:*:{self.account}:parameter/*dataall*",
                     f"arn:aws:ssm:*:{self.account}:parameter/*{resource_prefix}*",
-                    f"arn:aws:ecs:*:{self.account}:task-definition/{resource_prefix}-{envname}:*",
+                    f"arn:aws:ecs:*:{self.account}:task-definition/{resource_prefix}-{envname}-*",
                     f"arn:aws:iam::{self.account}:role/{resource_prefix}-{envname}-ecs-tasks-role",
                 ],
             )
