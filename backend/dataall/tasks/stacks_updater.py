872280f9106fcef744e1f226dd68f0f19849e8db
dlpzx <dlpzx@amazon.com>
2023-03-20 10:02:13 +0100

    Back to sequential updates in stack-updater

--- backend/dataall/tasks/stacks_updater.py
+++ backend/dataall/tasks/stacks_updater.py
@@ -25,50 +25,33 @@ def update_stacks(engine, envname):
         log.info(f'Found {len(all_environments)} environments, triggering update stack tasks...')
         environment: models.Environment
         for environment in all_environments:
-            update_stack(session, envname, environment.environmentUri)
+            update_stack(session=session, envname=envname, target_uri=environment.environmentUri, wait=True)
 
-        environments_being_updated = [True]
-        retries = 1
-        while any(environments_being_updated):
-            if retries > 1:
-                log.info("Update for environments is not complete, waiting for 30 seconds...")
-                time.sleep(30)
-            retries = retries + 1
-            environments_being_updated = []
-            for environment in all_environments:
-                env_running = check_stack(session, envname, environment.environmentUri)
-                log.info(f"Update for {environment.environmentUri} is not complete" if env_running else f"Update for {environment.environmentUri} is COMPLETE")
-                environments_being_updated.append(env_running)
-            if retries > 60:
-                log.info("Maximum number of retries exceeded (30mins), continuing task...")
-                break
-
-        log.info("Update for all environments COMPLETE or maximum number of retries exceeded")
-        log.info(f'Found {len(all_datasets)} datasets, triggering update stack tasks...')
+        log.info(f'Found {len(all_datasets)} datasets')
         dataset: models.Dataset
         for dataset in all_datasets:
-            update_stack(session, envname, dataset.datasetUri)
+            update_stack(session=session, envname=envname, target_uri=dataset.datasetUri, wait=False)
 
         return all_environments, all_datasets
 
 
-def check_stack(session, envname, target_uri):
-    stack: models.Stack = db.api.Stack.get_stack_by_target_uri(
-        session, target_uri=target_uri
-    )
-    log.info(f"Checking task for stack {stack.name}//{stack.stackUri}")
-    cluster_name = Parameter().get_parameter(env=envname, path='ecs/cluster/name')
-    return Ecs.is_task_running(cluster_name=cluster_name, started_by=f'awsworker-{stack.stackUri}')
-
-
-def update_stack(session, envname, target_uri):
+def update_stack(session, envname, target_uri, wait=False):
     stack: models.Stack = db.api.Stack.get_stack_by_target_uri(
         session, target_uri=target_uri
     )
     cluster_name = Parameter().get_parameter(env=envname, path='ecs/cluster/name')
     if not Ecs.is_task_running(cluster_name=cluster_name, started_by=f'awsworker-{stack.stackUri}'):
-        log.info(f"Updating stack {stack.name}//{stack.stackUri}")
         stack.EcsTaskArn = Ecs.run_cdkproxy_task(stack_uri=stack.stackUri)
+        if wait:
+            retries = 1
+            while Ecs.is_task_running(cluster_name=cluster_name, started_by=f'awsworker-{stack.stackUri}'):
+                log.info(f"Update for {stack.name}//{stack.stackUri} is not complete, waiting for 30 seconds...")
+                time.sleep(30)
+                retries = retries + 1
+                if retries > 30:
+                    log.info("Maximum number of retries exceeded, continuing task...")
+                    break
+            log.info(f"Update for {stack.name}//{stack.stackUri} COMPLETE or maximum number of retries exceeded")
     else:
         log.info(
             f'Stack update is already running... Skipping stack {stack.name}//{stack.stackUri}'
