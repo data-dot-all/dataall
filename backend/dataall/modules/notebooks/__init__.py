3f74ddb9d12d9c9e7cff9f862fd4eb7128b4d6af
Nikita Podshivalov <nikpodsh@amazon.com>
2023-03-27 16:21:47 +0200

    Moved TargetType to module initialization

--- backend/dataall/modules/notebooks/__init__.py
+++ backend/dataall/modules/notebooks/__init__.py
@@ -1,6 +1,7 @@
 """Contains the code related to SageMaker notebooks"""
 import logging
 
+from dataall.db.api import TargetType
 from dataall.modules.loader import ImportMode
 from dataall.modules.notebooks.db.repositories import NotebookRepository
 
@@ -13,6 +14,10 @@ class NotebookInterface:
     def initialize(self, modes):
         if ImportMode.API in modes:
             import dataall.modules.notebooks.api
+
+            from dataall.modules.notebooks.services.permissions import GET_NOTEBOOK, UPDATE_NOTEBOOK
+            TargetType("notebook", GET_NOTEBOOK, UPDATE_NOTEBOOK)
+
         if ImportMode.CDK in modes:
             import dataall.modules.notebooks.cdk
 
--- backend/dataall/modules/notebooks/services/__init__.py
+++ backend/dataall/modules/notebooks/services/__init__.py
@@ -2,10 +2,6 @@
 Contains the code needed for service layer.
 The service layer is a layer where all business logic is aggregated
 """
-
-from dataall.db.api import TargetType
 from dataall.modules.notebooks.services import services, permissions
 
 __all__ = ["services", "permissions"]
-
-TargetType("notebook", permissions.GET_NOTEBOOK, permissions.UPDATE_NOTEBOOK)
