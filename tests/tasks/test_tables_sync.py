d9844834646aa7564104ef184ca95d41f0ecbbb2
Nikita Podshivalov <nikpodsh@amazon.com>
2023-04-12 13:12:39 +0200

    Fixed and unignored test_tables_sync

--- tests/tasks/test_tables_sync.py
+++ tests/tasks/test_tables_sync.py
@@ -92,7 +92,13 @@ def table(org, env, db, sync_dataset):
     yield table
 
 
-def _test_tables_sync(db, org, env, sync_dataset, table, mocker):
+@pytest.fixture(scope='module', autouse=True)
+def permissions(db):
+    with db.scoped_session() as session:
+        yield dataall.db.api.Permission.init_permissions(session)
+
+
+def test_tables_sync(db, org, env, sync_dataset, table, mocker):
     mocker.patch(
         'dataall.aws.handlers.glue.Glue.list_glue_database_tables',
         return_value=[
@@ -147,7 +153,7 @@ def _test_tables_sync(db, org, env, sync_dataset, table, mocker):
         ],
     )
     mocker.patch(
-        'dataall.modules.datasets.tables_syncer.is_assumable_pivot_role', return_value=True
+        'dataall.modules.datasets.tasks.tables_syncer.is_assumable_pivot_role', return_value=True
     )
     mocker.patch(
         'dataall.aws.handlers.glue.Glue.grant_principals_all_table_permissions',
