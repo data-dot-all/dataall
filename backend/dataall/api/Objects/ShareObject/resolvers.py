4a9d67234523f8672948a4c4be4dc76e82291cfa
nikpodsh <124577300+nikpodsh@users.noreply.github.com>
2023-04-25 16:27:20 +0200

    Fixed unresolved reference for ShareObjectPermission (#418)
    ### Feature or Bugfix
    - Bugfix
    
    ### Detail
    Working on datasets, I found that there was no `ApproversAndRequesters`
    value in `ShareObjectPermission`. Whenever the code reaches this point
    there will be an error.
    
    I added the other value to avoid an exception. But this is just for
    awareness.
    We might need to add `ApproversAndRequesters` to `ShareObjectPermission`
    I also considered that It might be dead code that we could delete.
    
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache 2.0 license.

--- backend/dataall/api/Objects/ShareObject/resolvers.py
+++ backend/dataall/api/Objects/ShareObject/resolvers.py
@@ -222,25 +222,17 @@ def resolve_user_role(context: Context, source: models.ShareObject, **kwargs):
         return None
     with context.engine.scoped_session() as session:
         dataset: models.Dataset = db.api.Dataset.get_dataset_by_uri(session, source.datasetUri)
-        if dataset and dataset.stewards in context.groups:
-            return ShareObjectPermission.Approvers.value
-        if (
-            source.owner == context.username
-            or source.principalId in context.groups
+        if dataset and (
+            dataset.stewards in context.groups
             or dataset.owner == context.username
             or dataset.SamlAdminGroupName in context.groups
         ):
-            return ShareObjectPermission.Requesters.value
-        if (
-            dataset and dataset.stewards in context.groups
-            and (
-                source.owner == context.username
-                or source.principalId in context.groups
-                or dataset.owner == context.username
-                or dataset.SamlAdminGroupName in context.groups
-            )
+            return ShareObjectPermission.Approvers.value
+        if dataset and (
+            source.owner == context.username
+            or source.principalId in context.groups
         ):
-            return ShareObjectPermission.ApproversAndRequesters.value
+            return ShareObjectPermission.Requesters.value
         else:
             return ShareObjectPermission.NoPermission.value
 
