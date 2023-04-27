9f2748e8869bfccd4264fb41112b1c01ff9ca510
Nikita Podshivalov <nikpodsh@amazon.com>
2023-04-27 18:21:46 +0200

    Added dataset data policy to import it with other stacks

--- backend/dataall/modules/datasets/cdk/__init__.py
+++ backend/dataall/modules/datasets/cdk/__init__.py
@@ -1,3 +1,4 @@
-from dataall.modules.datasets.cdk import dataset_stack, databrew_policy, glue_policy, lakeformation_policy
+from dataall.modules.datasets.cdk import dataset_stack, databrew_policy, glue_policy, lakeformation_policy, \
+    dataset_policy
 
-__all__ = ["dataset_stack", "databrew_policy", "glue_policy", "lakeformation_policy"]
+__all__ = ["dataset_stack", "databrew_policy", "glue_policy", "lakeformation_policy", "dataset_policy"]
