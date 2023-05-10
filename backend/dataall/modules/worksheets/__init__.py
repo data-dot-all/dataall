aebea6a69966b59c383a7ca50399b56d4cba5444
Balint David <dbalintx@amazon.com>
2023-05-10 14:20:43 +0200

    implementing moduleinterface for worksheets

--- backend/dataall/modules/worksheets/__init__.py
+++ backend/dataall/modules/worksheets/__init__.py
@@ -1,1 +1,24 @@
-#TODO
+"""Contains the code related to worksheets"""
+import logging
+
+from dataall.modules.loader import ImportMode, ModuleInterface
+from dataall.modules.worksheets.db.models import Worksheet
+
+log = logging.getLogger(__name__)
+
+
+class WorksheetApiModuleInterface(ModuleInterface):
+    """Implements ModuleInterface for worksheet GraphQl lambda"""
+
+    @classmethod
+    def is_supported(cls, modes):
+        return ImportMode.API in modes
+
+    def __init__(self):
+        from dataall.api.Objects.Feed.registry import FeedRegistry, FeedDefinition
+
+        import dataall.modules.worksheets.api
+
+        FeedRegistry.register(FeedDefinition("Worksheet", Worksheet))
+
+        log.info("API of worksheets has been imported")
