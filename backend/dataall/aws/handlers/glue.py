3b10f32bee42d338d04502e4db0599b38b1ebc86
Nikita Podshivalov <nikpodsh@amazon.com>
2023-05-17 11:13:24 +0200

    Deleted dead code

--- backend/dataall/aws/handlers/glue.py
+++ backend/dataall/aws/handlers/glue.py
@@ -52,12 +52,6 @@ class Glue:
             raise e
 
     @staticmethod
-    def get_database_arn(**data):
-        return 'arn:aws:glue:{}:{}:database/{}'.format(
-            data.get('region', 'eu-west-1'), data.get('accountid'), data.get('database')
-        )
-
-    @staticmethod
     def database_exists(**data):
         accountid = data['accountid']
         database = data.get('database', 'UnknownDatabaseName')
@@ -123,89 +117,6 @@ class Glue:
             return None
 
     @staticmethod
-    def _create_table(**data):
-        accountid = data['accountid']
-        region = data.get('region', 'eu-west-1')
-        database = data.get('database', 'UnknownDatabaseName')
-
-        session = SessionHelper.remote_session(accountid=accountid)
-        glue = session.client('glue', region_name=region)
-        log.info(
-            'Creating table {} in database {}'.format(
-                data['tablename'], data['database']
-            )
-        )
-        if not Glue.database_exists(
-            database=database, region=region, accountid=accountid
-        ):
-            Glue.create_database(accountid, database, region, None)
-        if 'table_input' not in data:
-            table_input = {
-                'Name': data['tablename'],
-                'Description': data.get('Description', 'Not available'),
-                'Parameters': {'classification': 'csv', 'skip.header.line.count': '1'},
-                'StorageDescriptor': {
-                    'Columns': [
-                        {'Name': c['Name'], 'Type': c['Type']}
-                        for c in data.get('columns')
-                    ],
-                    'Location': data.get('location'),
-                    'InputFormat': 'org.apache.hadoop.mapred.TextInputFormat',
-                    'OutputFormat': 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat',
-                    'SerdeInfo': {
-                        'SerializationLibrary': 'org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe',
-                        'Parameters': {
-                            'serialization.format': ',',
-                            'field.delim': ',',
-                            'escape.delim': '\\',
-                        },
-                    },
-                },
-                'TableType': 'EXTERNAL_TABLE',
-                'PartitionKeys': data.get('partition_keys') or [],
-            }
-        else:
-            table_input = data['table_input']
-
-        found_table = Glue.table_exists(**data)
-
-        if not found_table:
-            response = glue.create_table(
-                CatalogId=accountid,
-                DatabaseName=data.get('database'),
-                TableInput=table_input,
-            )
-            log.info(f'Successfully Created table {table_input} on account {accountid}')
-            return response
-
-        else:
-
-            if Glue.is_resource_link(found_table):
-
-                log.info(
-                    f'Table is a Resource Link {found_table} '
-                    f'on account {accountid} and is managed by source account'
-                )
-                return found_table
-
-            elif Glue.is_resource_link(table_input):
-
-                return Glue.delete_table_and_create_resourcelink(
-                    glue, database, accountid, table_input
-                )
-
-            else:
-                response = glue.update_table(
-                    CatalogId=accountid,
-                    DatabaseName=data.get('database'),
-                    TableInput=table_input,
-                )
-                log.info(
-                    f'Successfully Updated table {found_table} on account {accountid}'
-                )
-                return response
-
-    @staticmethod
     def delete_table(accountid, region, database, tablename):
         session = SessionHelper.remote_session(accountid=accountid)
         client = session.client('glue', region_name=region)
@@ -264,52 +175,6 @@ class Glue:
             raise e
 
     @staticmethod
-    def is_resource_link(table_input: dict):
-        """
-        Verifies if a Glue table or Glue table input contains the block "TargetTable"
-        if it is the case it means it is a Resource Link
-        to a shared table by Lake Formation cross account or from the same account
-        :param table_input:
-        :return:
-        """
-        if 'TargetTable' in table_input.keys():
-            log.info(
-                f"Table {table_input['Name']} is a resource link "
-                f"from account {table_input['TargetTable']['CatalogId']} and will not be updated"
-            )
-            return True
-        return False
-
-    @staticmethod
-    def delete_table_and_create_resourcelink(glue, database, accountid, table_input):
-        """
-        When table exists before Lake Formation introduction it needs to be deleted
-        And transformed to a resource link
-        :param glue:
-        :param database:
-        :param accountid:
-        :param table_input:
-        :return:
-        """
-        try:
-            glue.delete_table(
-                CatalogId=accountid, DatabaseName=database, Name=table_input['Name']
-            )
-            log.debug(
-                f'Successfully Deleted table {table_input} on account {accountid}'
-            )
-            response = glue.create_table(
-                CatalogId=accountid, DatabaseName=database, TableInput=table_input
-            )
-            log.info(f'Successfully Changed table to resource link {response}')
-            return response
-        except ClientError as e:
-            log.warning(
-                f'Failed to change table to resource link {table_input} due to: {e}'
-            )
-            raise e
-
-    @staticmethod
     def delete_database(**data):
         accountid = data['accountid']
         region = data['region']
@@ -329,41 +194,6 @@ class Glue:
             log.error(
                 f'Could not delete database {database} '
                 f'in account {accountid} '
-                f'due to: {e}'
-            )
-            raise e
-
-    @staticmethod
-    def batch_delete_tables(**data):
-        accountid = data['accountid']
-        region = data['region']
-        database = data['database']
-        tables = data['tables']
-
-        if not tables:
-            log.info('No tables to delete exiting method...')
-            return
-
-        log.info(f'Batch deleting tables: {tables}')
-        try:
-            session = SessionHelper.remote_session(accountid=accountid)
-            glue = session.client('glue', region_name=region)
-            if Glue.database_exists(
-                accountid=accountid,
-                region=region,
-                database=database,
-            ):
-                glue.batch_delete_table(
-                    CatalogId=accountid, DatabaseName=database, TablesToDelete=tables
-                )
-                log.debug(
-                    f'Batch deleted tables {len(tables)} from database {database} successfully'
-                )
-            return True
-        except ClientError as e:
-            log.error(
-                f'Could not batch delete tables {tables} '
-                f'in database {accountid}://{database} '
                 f'due to: {e}'
             )
             raise e
