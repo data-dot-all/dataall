bf3d739480cf1b2cecdad82481aa4dde5c78d6c9
Nikita Podshivalov <nikpodsh@amazon.com>
2023-05-30 16:24:31 +0200

    Removed missed dashboard code

--- backend/dataall/tasks/catalog_indexer.py
+++ backend/dataall/tasks/catalog_indexer.py
@@ -5,9 +5,7 @@ from abc import ABC
 from typing import List
 
 from dataall.db import get_engine
-from dataall.modules.dashboards.db.models import Dashboard
 from dataall.modules.loader import load_modules, ImportMode
-from dataall.modules.dashboards.indexers.dashboard_indexer import DashboardIndexer
 from dataall.utils.alarm_service import AlarmService
 
 root = logging.getLogger()
@@ -37,13 +35,6 @@ def index_objects(engine):
         with engine.scoped_session() as session:
             for indexer in _indexers:
                 indexed_objects_counter += indexer.index(session)
-
-            all_dashboards: [Dashboard] = session.query(Dashboard).all()
-            log.info(f'Found {len(all_dashboards)} dashboards')
-            dashboard: Dashboard
-            for dashboard in all_dashboards:
-                DashboardIndexer.upsert(session=session, dashboard_uri=dashboard.dashboardUri)
-                indexed_objects_counter = indexed_objects_counter + 1
 
             log.info(f'Successfully indexed {indexed_objects_counter} objects')
             return indexed_objects_counter
