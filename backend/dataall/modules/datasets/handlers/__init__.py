4cc4e0f16ccab1ac18b06e1b05bd0060f6ae51b5
Nikita Podshivalov <nikpodsh@amazon.com>
2023-06-02 10:20:54 +0200

    Handler was not used

--- backend/dataall/modules/datasets/handlers/__init__.py
+++ backend/dataall/modules/datasets/handlers/__init__.py
@@ -6,9 +6,7 @@ from dataall.modules.datasets.handlers import (
     glue_table_sync_handler,
     glue_table_handler,
     glue_profiling_handler,
-    s3_folder_creator_handler,
     glue_dataset_handler
 )
 
-__all__ = ["glue_table_sync_handler", "glue_table_handler", "glue_profiling_handler", "s3_folder_creator_handler",
-           "glue_dataset_handler"]
+__all__ = ["glue_table_sync_handler", "glue_table_handler", "glue_profiling_handler", "glue_dataset_handler"]
