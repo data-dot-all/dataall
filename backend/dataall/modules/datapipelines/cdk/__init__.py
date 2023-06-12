39f633ac8a8d59e9c8fc69ae9635f38e15575073
Balint David <dbalintx@amazon.com>
2023-06-12 16:59:11 +0200

    readd pipelinestack

--- backend/dataall/modules/datapipelines/cdk/__init__.py
+++ backend/dataall/modules/datapipelines/cdk/__init__.py
@@ -1,10 +1,11 @@
 from dataall.modules.datapipelines.cdk import datapipelines_cdk_cli_wrapper_extension, datapipelines_cdk_pipeline, \
-    datapipelines_stepfunctions_policy, datapipelines_lambda_policy, datapipelines_codestar_policy
+    datapipelines_stepfunctions_policy, datapipelines_lambda_policy, datapipelines_codestar_policy, datapipelines_pipeline
 
 __all__ = [
     'datapipelines_cdk_cli_wrapper_extension',
     'datapipelines_cdk_pipeline',
     'datapipelines_stepfunctions_policy',
     'datapipelines_lambda_policy',
-    'datapipelines_codestar_policy'
+    'datapipelines_codestar_policy',
+    'datapipelines_pipeline'
 ]
