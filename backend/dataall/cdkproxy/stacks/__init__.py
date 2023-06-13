b36b36fcfc174f66a47137e32bb97880a1f283d2
dlpzx <dlpzx@amazon.com>
2023-06-13 18:55:36 +0200

    Added testing of SGM as standalone

--- backend/dataall/cdkproxy/stacks/__init__.py
+++ backend/dataall/cdkproxy/stacks/__init__.py
@@ -1,10 +1,11 @@
-from .environment import EnvironmentSetup
+from .environment import EnvironmentSetup, EnvironmentStackExtension
 from .pipeline import PipelineStack
 from .manager import stack, instanciate_stack, StackManager
 from .redshift_cluster import RedshiftStack
 
 __all__ = [
     'EnvironmentSetup',
+    'EnvironmentStackExtension',
     'StackManager',
     'stack',
     'StackManager',
--- tests/cdkproxy/test_environment_stack.py
+++ tests/cdkproxy/test_environment_stack.py
@@ -1,20 +1,29 @@
 import json
 
 import pytest
-from aws_cdk import App
+from aws_cdk import App, Stack
+from aws_cdk.assertions import Template
 
 from dataall.cdkproxy.stacks import EnvironmentSetup
 
+
+# @pytest.fixture(scope='function', autouse=True)
+# def patch_methods_sagemaker_studio_extension(mocker):
+#     # TODO = WAYS OF TESTING EXTENSIONS outside of environment testing
+#     mocker.patch(
+#         'dataall.aws.handlers.sts.SessionHelper.get_cdk_look_up_role_arn',
+#         return_value="arn:aws:iam::1111111111:role/cdk-hnb659fds-lookup-role-1111111111-eu-west-1",
+#     )
+#     mocker.patch(
+#         'dataall.modules.mlstudio.aws.ec2_client.EC2.check_default_vpc_exists',
+#         return_value=False,
+#     )
+
 @pytest.fixture(scope='function', autouse=True)
-def patch_methods_sagemaker_studio_extension(mocker):
-    # TODO = WAYS OF TESTING EXTENSIONS outside of environment testing
+def patch_method_extensions(mocker):
     mocker.patch(
-        'dataall.aws.handlers.sts.SessionHelper.get_cdk_look_up_role_arn',
-        return_value="arn:aws:iam::1111111111:role/cdk-hnb659fds-lookup-role-1111111111-eu-west-1",
-    )
-    mocker.patch(
-        'dataall.modules.mlstudio.aws.ec2_client.EC2.check_default_vpc_exists',
-        return_value=False,
+        'dataall.cdkproxy.stacks.environment.EnvironmentSetup.register',
+        return_value=True,
     )
 
 @pytest.fixture(scope='function', autouse=True)
@@ -54,7 +63,7 @@ def patch_methods(mocker, db, env, another_group, permissions):
     )
     mocker.patch(
         'dataall.aws.handlers.sts.SessionHelper.get_external_id_secret',
-        return_value='*****',
+        return_value='secretIdvalue',
     )
 
 
@@ -65,8 +74,19 @@ def template(env):
     return json.dumps(app.synth().get_stack_by_name('Environment').template)
 
 
-def test_resources_created(template, env):
-    assert 'AWS::S3::Bucket' in template
-    assert 'AWS::IAM::Role' in template
-    assert 'AWS::Lambda::Function' in template
-    assert 'AWS::IAM::Policy' in template
+def test_resources_created(template, env, patch_method_extensions):
+    app = App()
+
+    # Create the Stack
+    stack = EnvironmentSetup(app, 'Environment', target_uri=env.environmentUri)
+
+    # Prepare the stack for assertions.
+    template = Template.from_stack(stack)
+
+    # Assert that we have created:
+    # TODO: assertions allow us more tests than these, we should enhance the assertions
+    template.resource_count_is("AWS::S3::Bucket", 1)
+    template.resource_count_is("AWS::IAM::Rolet", 1)
+    template.resource_count_is("AWS::Lambda::Function", 1)
+    template.resource_count_is("AWS::IAM::Policy", 1)
+
