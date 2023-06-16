3ee30005bb01fc5788c63a2f2cc81a1ee03a8c5f
Nikita Podshivalov <nikpodsh@amazon.com>
2023-06-16 14:58:43 +0200

    Disable and skip module test directories for modules that are inactive

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,27 @@
 import os
 import pytest
 import dataall
+from dataall.core.config import config
 from dataall.modules.loader import load_modules, ImportMode
 
 load_modules(modes=[ImportMode.HANDLERS, ImportMode.API, ImportMode.CDK])
 ENVNAME = os.environ.get('envname', 'pytest')
+
+collect_ignore_glob = []
+
+
+def ignore_module_tests_if_not_active():
+    """
+    Ignores tests of the modules that are turned off.
+    It uses the collect_ignore_glob hook
+    """
+    modules = config.get_property('modules', {})
+    for name, props in modules.items():
+        if not props["active"]:
+            collect_ignore_glob.append(os.path.join('modules', f'{name}*'))
+
+
+ignore_module_tests_if_not_active()
 
 
 @pytest.fixture(scope='module')
