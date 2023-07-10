cc94e62344979e177edf5cadc816e8482c245325
Nikita Podshivalov <nikpodsh@amazon.com>
2023-07-10 12:01:28 +0200

    Delete unused files

--- backend/dataall/db/models/__init__.py
+++ backend/dataall/db/models/__init__.py
@@ -12,7 +12,6 @@ from .Notification import Notification, NotificationType
 from .Organization import Organization
 from .OrganizationGroup import OrganizationGroup
 from .Stack import Stack
-from .Tag import Tag, ItemTags, updateObjectTags
 from .Task import Task
 from .Tenant import Tenant
 from .User import User
