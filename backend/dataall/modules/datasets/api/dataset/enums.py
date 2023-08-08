3758e6840cea5d46645da37a6f34bcc568542089
dlpzx <71252798+dlpzx@users.noreply.github.com>
2023-08-08 13:26:36 +0200

    Modularization: clean up base (#638)
    ### Feature or Bugfix
    - Refactoring
    
    ### Detail
    - move environment CFN custom resources from `base` to
    `core/environment`
    - fix typo in dockerfile mkdir for jars of profiling job
    
    ### Relates
    
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache 2.0 license.

--- backend/dataall/modules/datasets/api/dataset/enums.py
+++ backend/dataall/modules/datasets/api/dataset/enums.py
@@ -21,3 +21,25 @@ class DatasetSortField(GraphQLEnumMapper):
     label = 'label'
     created = 'created'
     updated = 'updated'
+
+
+class Language(GraphQLEnumMapper):
+    English = 'English'
+    French = 'French'
+    German = 'German'
+
+
+class Topic(GraphQLEnumMapper):
+    Finances = 'Finances'
+    HumanResources = 'HumanResources'
+    Products = 'Products'
+    Services = 'Services'
+    Operations = 'Operations'
+    Research = 'Research'
+    Sales = 'Sales'
+    Orders = 'Orders'
+    Sites = 'Sites'
+    Energy = 'Energy'
+    Customers = 'Customers'
+    Misc = 'Misc'
+
