983cf06e5d99104856a2769947b28faeafb80b7c
dlpzx <dlpzx@amazon.com>
2023-08-08 11:36:02 +0200

    Fix imports in env_role modules

--- backend/dataall/modules/datasets/cdk/env_role_dataset_s3_policy.py
+++ backend/dataall/modules/datasets/cdk/env_role_dataset_s3_policy.py
@@ -1,7 +1,7 @@
 from typing import List
 from aws_cdk import aws_iam as iam
 
-from dataall.base.cdkproxy.stacks.policies.data_policy import S3Policy
+from dataall.core.environment.cdk.env_role_core_policies.data_policy import S3Policy
 from dataall.modules.dataset_sharing.aws.kms_client import KmsClient
 from dataall.modules.datasets_base.db.dataset_repository import DatasetRepository
 from dataall.modules.datasets_base.db.models import Dataset
