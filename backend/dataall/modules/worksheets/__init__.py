2c61e72f003cce71d798a8c93dc1ef5c38e96188
dlpzx <dlpzx@amazon.com>
2023-08-16 08:59:10 +0200

    Remove Feed dependency from Worksheets

--- backend/dataall/modules/worksheets/__init__.py
+++ backend/dataall/modules/worksheets/__init__.py
@@ -1,10 +1,8 @@
 """Contains the code related to worksheets"""
 import logging
-from typing import List, Type
 
 from dataall.core.environment.services.environment_resource_manager import EnvironmentResourceManager
 from dataall.base.loader import ImportMode, ModuleInterface
-from dataall.modules.feed import FeedApiModuleInterface
 from dataall.modules.worksheets.db.models import Worksheet
 from dataall.modules.worksheets.db.worksheets_repository import WorksheetRepository
 
@@ -18,16 +16,9 @@ class WorksheetApiModuleInterface(ModuleInterface):
     def is_supported(modes):
         return ImportMode.API in modes
 
-    @staticmethod
-    def depends_on() -> List[Type['ModuleInterface']]:
-        return [FeedApiModuleInterface]
-
     def __init__(self):
-        from dataall.modules.feed.api.registry import FeedRegistry, FeedDefinition
 
         import dataall.modules.worksheets.api
-
-        FeedRegistry.register(FeedDefinition("Worksheet", Worksheet))
 
         EnvironmentResourceManager.register(WorksheetRepository())
 
