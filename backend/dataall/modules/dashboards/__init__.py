a4670654c5422008568b5f1d9a60fb45583e9d54
dlpzx <dlpzx@amazon.com>
2023-08-18 13:06:30 +0200

    Resolve conflicts with modularization main

--- backend/dataall/modules/dashboards/__init__.py
+++ backend/dataall/modules/dashboards/__init__.py
@@ -3,7 +3,7 @@ import logging
 from typing import Set, List, Type
 
 from dataall.core.environment.services.environment_resource_manager import EnvironmentResourceManager
-from dataall.modules.dashboards.db.dashboard_repository import DashboardRepository
+from dataall.modules.dashboards.db.dashboard_repositories import DashboardRepository
 from dataall.modules.dashboards.db.dashboard_models import Dashboard
 from dataall.base.loader import ImportMode, ModuleInterface
 
