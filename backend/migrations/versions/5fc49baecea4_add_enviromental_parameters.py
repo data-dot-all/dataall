e0b287f82bac86506437abe8dabf56f6f32fbf8b
Noah Paige <69586985+noah-paige@users.noreply.github.com>
2023-08-23 09:29:46 -0400

    Fix Env Parameters Migration Script (#692)
    ### Feature or Bugfix
    <!-- please choose -->
    - Bugfix
    
    
    ### Detail
    - Resolve errors in migration script for environemnt parameters when
    upgrading from data.all v1.6.2 with pre-existing environments to
    beta-2.0
    
    ### Relates
    - #691 
    
    ### Security
    Please answer the questions below briefly where applicable, or write
    `N/A`. Based on
    [OWASP 10](https://owasp.org/Top10/en/).
    
    - Does this PR introduce or modify any input fields or queries - this
    includes
    fetching data from storage outside the application (e.g. a database, an
    S3 bucket)?
      - Is the input sanitized?
    - What precautions are you taking before deserializing the data you
    consume?
      - Is injection prevented by parametrizing queries?
      - Have you ensured no `eval` or similar functions are used?
    - Does this PR introduce any functionality or component that requires
    authorization?
    - How have you ensured it respects the existing AuthN/AuthZ mechanisms?
      - Are you logging failed auth attempts?
    - Are you using or adding any cryptographic features?
      - Do you use a standard proven implementations?
      - Are the used keys controlled by the customer? Where are they stored?
    - Are you introducing any new policies/roles/users?
      - Have you used the least-privilege principle? How?
    
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache 2.0 license.
    
    ---------
    
    Co-authored-by: dlpzx <dlpzx@amazon.com>

--- backend/migrations/versions/5fc49baecea4_add_enviromental_parameters.py
+++ backend/migrations/versions/5fc49baecea4_add_enviromental_parameters.py
@@ -5,7 +5,6 @@ Revises: e1cd4927482b
 Create Date: 2023-02-20 14:28:13.331670
 
 """
-import sqlalchemy as sa
 
 from typing import List
 
@@ -14,12 +13,7 @@ from sqlalchemy import Boolean, Column, String, orm
 from sqlalchemy.ext.declarative import declarative_base
 
 from dataall.core.environment.db.environment_models import EnvironmentGroup
-from dataall.core.permissions.db.permission_repositories import Permission
-from dataall.core.permissions.db.resource_policy_repositories import ResourcePolicy
 from dataall.base.db import Resource
-from dataall.core.permissions.db.permission_models import PermissionType, ResourcePolicyPermission, \
-    TenantPolicyPermission
-from dataall.modules.datasets.services.dataset_permissions import LIST_ENVIRONMENT_DATASETS, CREATE_DATASET
 
 # revision identifiers, used by Alembic.
 revision = "5fc49baecea4"
@@ -28,20 +22,6 @@ branch_labels = None
 depends_on = None
 
 Base = declarative_base()
-
-UNUSED_RESOURCE_PERMISSIONS = [
-    'LIST_DATASETS', 'LIST_DATASET_TABLES', 'LIST_DATASET_SHARES', 'SUMMARY_DATASET',
-    'UPLOAD_DATASET', 'URL_DATASET', 'STACK_DATASET', 'SUBSCRIPTIONS_DATASET',
-    'CREATE_DATASET_TABLE', 'LIST_PIPELINES', 'DASHBOARD_URL', 'GET_REDSHIFT_CLUSTER',
-    'SHARE_REDSHIFT_CLUSTER', 'DELETE_REDSHIFT_CLUSTER', 'REBOOT_REDSHIFT_CLUSTER', 'RESUME_REDSHIFT_CLUSTER',
-    'PAUSE_REDSHIFT_CLUSTER', 'ADD_DATASET_TO_REDSHIFT_CLUSTER', 'LIST_REDSHIFT_CLUSTER_DATASETS',
-    'REMOVE_DATASET_FROM_REDSHIFT_CLUSTER', 'ENABLE_REDSHIFT_TABLE_COPY', 'DISABLE_REDSHIFT_TABLE_COPY',
-    'GET_REDSHIFT_CLUSTER_CREDENTIALS', 'CREATE_REDSHIFT_CLUSTER', 'LIST_ENVIRONMENT_REDSHIFT_CLUSTERS'
-]
-
-UNUSED_TENANT_PERMISSIONS = [
-    'MANAGE_REDSHIFT_CLUSTERS'
-]
 
 
 class Environment(Resource, Base):
@@ -55,16 +35,16 @@ class Environment(Resource, Base):
 
 
 class EnvironmentParameter(Base):
-    __tablename__ = "environment_parameters"
+    __tablename__ = 'environment_parameters'
     environmentUri = Column(String, primary_key=True)
-    paramKey = Column(String, primary_key=True),
-    paramValue = Column(String, nullable=True)
+    key = Column('paramKey', String, primary_key=True)
+    value = Column('paramValue', String, nullable=True)
 
-
-class SagemakerNotebook(Resource, Base):
-    __tablename__ = "sagemaker_notebook"
-    environmentUri = Column(String, nullable=False)
-    notebookUri = Column(String, primary_key=True)
+    def __init__(self, env_uri, key, value):
+        super().__init__()
+        self.environmentUri = env_uri
+        self.key = key
+        self.value = value
 
 
 def upgrade():
@@ -123,13 +103,9 @@ def upgrade():
 
         session.commit()
 
-        migrate_groups_permissions(session)
-        delete_unused_permissions(session)
-
-        op.drop_table("tenant_administrator")
-
     except Exception as ex:
         print(f"Failed to execute the migration script due to: {ex}")
+        raise ex
 
 
 def downgrade():
@@ -152,35 +128,41 @@ def downgrade():
         op.add_column("environment", Column("warehousesEnabled", Boolean, default=True))
 
         print("Filling environment table with parameters rows...")
-        params = session.query(EnvironmentParameter).all()
+        envs_all = session.query(Environment).all()
         envs = []
-        for param in params:
-            print(param)
-            envs.append(Environment(
-                environmentUri=param.environmentUri,
-                notebooksEnabled=params["notebooksEnabled"] == "true",
-                mlStudiosEnabled=params["mlStudiosEnabled"] == "true",
-                pipelinesEnabled=params["pipelinesEnabled"] == "true",
-                dashboardsEnabled=params["dashboardsEnabled"] == "true",
-                warehousesEnabled=params["warehousesEnabled"] == "true",
-            ))
+        for env in envs_all:
+            params = (
+                session.query(EnvironmentParameter)
+                .filter(EnvironmentParameter.environmentUri == EnvironmentParameter.environmentUri)
+            )
+            env_params = {}
+            for param in params:
+                env_params[param.key] = param.value == "true"
+            print(env_params)
 
-        save_deleted_permissions(session)
+            env.notebooksEnabled = env_params.get("notebooksEnabled", False)
+            env.mlStudiosEnabled = env_params.get("mlStudiosEnabled", False)
+            env.pipelinesEnabled = env_params.get("pipelinesEnabled", False)
+            env.dashboardsEnabled = env_params.get("dashboardsEnabled", False)
+            env.warehousesEnabled = env_params.get("warehousesEnabled", False)
+            session.commit()
 
-        session.add_all(envs)
         print("Dropping environment_parameter table...")
         op.drop_table("environment_parameters")
 
+        session.commit()
+
     except Exception as ex:
         print(f"Failed to execute the rollback script due to: {ex}")
+        raise ex
 
 
 def _add_param_if_exists(params: List[EnvironmentParameter], env: Environment, key, val) -> None:
     if val is not None:
         params.append(EnvironmentParameter(
-            environmentUri=env.environmentUri,
-            paramKey=key,
-            paramValue=str(val).lower()
+            env.environmentUri,
+            key,
+            str(val).lower()
         ))
 
 
@@ -194,59 +176,3 @@ def create_foreign_key_to_env(op, table: str):
 
 def find_all_groups(session):
     return session.query(EnvironmentGroup).all()
-
-
-def migrate_groups_permissions(session):
-    """
-    Adds new permission if the old exist. needed to get rid of old hacks in the code
-    """
-    permissions = [CREATE_DATASET, LIST_ENVIRONMENT_DATASETS]
-
-    groups = find_all_groups(session)
-    for group in groups:
-        new_perms = []
-        for existed, to_add in permissions:
-            if not ResourcePolicy.has_group_resource_permission(
-                session,
-                group_uri=group,
-                permission_name=existed,
-                resource_uri=group.environmentUri,
-            ):
-                new_perms.append(to_add)
-
-        if new_perms:
-            ResourcePolicy.attach_resource_policy(
-                session=session,
-                group=group.groupUri,
-                permissions=new_perms,
-                resource_uri=group.environmentUri,
-                resource_type=Environment.__name__
-            )
-
-
-def delete_unused_permissions(session):
-    for name in UNUSED_RESOURCE_PERMISSIONS:
-        perm = Permission.get_permission_by_name(session, name, PermissionType.RESOURCE.value)
-        (
-            session.query(ResourcePolicyPermission)
-            .filter(ResourcePolicyPermission.permissionUri == perm.permissionUri)
-            .delete()
-        )
-        session.delete(perm)
-
-    for name in UNUSED_TENANT_PERMISSIONS:
-        perm = Permission.get_permission_by_name(session, name, PermissionType.TENANT.value)
-        (
-            session.query(TenantPolicyPermission)
-            .filter(TenantPolicyPermission.permissionUri == perm.permissionUri)
-            .delete()
-        )
-        session.delete(perm)
-
-
-def save_deleted_permissions(session):
-    for name in UNUSED_RESOURCE_PERMISSIONS:
-        Permission.save_permission(session, name, name, PermissionType.RESOURCE.value)
-
-    for name in UNUSED_TENANT_PERMISSIONS:
-        Permission.save_permission(session, name, name, PermissionType.TENANT.value)
