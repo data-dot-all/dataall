f0e4dad6b132db053ea68c40a93e9ec097fc7aa8
dlpzx <dlpzx@amazon.com>
2023-08-29 11:26:41 +0200

    fix loader checks and implement CDK_CLI_WRAPPER import mode

--- backend/dataall/modules/datasets_base/__init__.py
+++ backend/dataall/modules/datasets_base/__init__.py
@@ -5,4 +5,12 @@ from dataall.base.loader import ModuleInterface, ImportMode
 class DatasetBaseModuleInterface(ModuleInterface):
     @staticmethod
     def is_supported(modes: Set[ImportMode]) -> bool:
-        return True
+        supported_modes = [
+            ImportMode.API,
+            ImportMode.CDK,
+            ImportMode.HANDLERS,
+            ImportMode.STACK_UPDATER_TASK,
+            ImportMode.CATALOG_INDEXER_TASK
+        ]
+        supported = [x in supported_modes for x in modes]
+        return True in supported
