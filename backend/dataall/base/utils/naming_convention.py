ddd250681d339826766a4483266fc865838828f3
dlpzx <dlpzx@amazon.com>
2023-09-26 14:31:21 +0200

    Avoid duplications in naming_convention utility

--- backend/dataall/base/utils/naming_convention.py
+++ backend/dataall/base/utils/naming_convention.py
@@ -4,8 +4,6 @@ from .slugify import slugify
 
 
 class NamingConventionPattern(Enum):
-    def __str__(self):
-        return self.value
 
     S3 = {'regex': '[^a-zA-Z0-9-]', 'separator': '-', 'max_length': 63}
     IAM = {'regex': '[^a-zA-Z0-9-_]', 'separator': '-', 'max_length': 63}
@@ -26,58 +24,14 @@ class NamingConventionService:
     ):
         self.target_label = target_label
         self.target_uri = target_uri
-        self.service = pattern
+        self.service = pattern.name
         self.resource_prefix = resource_prefix
 
     def build_compliant_name(self) -> str:
         """
         Builds a compliant AWS resource name
         """
-        if self.service == NamingConventionPattern.S3:
-            regex = self.service.S3.value['regex']
-            return self.build_s3_compliant_name(regex)
-        elif self.service == NamingConventionPattern.IAM:
-            regex = self.service.IAM.value['regex']
-            return self.build_iam_compliant_name(regex)
-        elif self.service == NamingConventionPattern.GLUE:
-            regex = self.service.GLUE.value['regex']
-            return self.build_glue_compliant_name(regex)
-        elif self.service == NamingConventionPattern.GLUE_ETL:
-            regex = self.service.GLUE_ETL.value['regex']
-            return self.build_glue_etl_compliant_name(regex)
-        elif self.service == NamingConventionPattern.NOTEBOOK:
-            regex = self.service.NOTEBOOK.value['regex']
-            return self.build_notebook_compliant_name(regex)
-        elif self.service == NamingConventionPattern.OPENSEARCH:
-            regex = self.service.OPENSEARCH.value['regex']
-            return self.build_opensearch_compliant_name(regex)
-        elif self.service == NamingConventionPattern.OPENSEARCH_SERVERLESS:
-            regex = self.service.OPENSEARCH_SERVERLESS.value['regex']
-            return self.build_opensearch_serverless_compliant_name(regex)
-        else:
-            regex = self.service.DEFAULT.value['regex']
-            return self.build_default_compliant_name(regex)
-
-    def build_s3_compliant_name(self, regex) -> str:
-        return f"{slugify(self.resource_prefix + '-' + self.target_label[:(self.service.S3.value['max_length'] - len(self.resource_prefix + self.target_uri))] + '-' + self.target_uri, regex_pattern=fr'{regex}', separator=self.service.S3.value['separator'], lowercase=True)}"
-
-    def build_iam_compliant_name(self, regex) -> str:
-        return f"{slugify(self.resource_prefix + '-' + self.target_label[:(self.service.IAM.value['max_length'] - len(self.resource_prefix + self.target_uri))] + '-' + self.target_uri, regex_pattern=fr'{regex}', separator=self.service.IAM.value['separator'], lowercase=True)}"
-
-    def build_glue_compliant_name(self, regex) -> str:
-        return f"{slugify(self.resource_prefix + '-' + self.target_label[:(self.service.GLUE.value['max_length'] - len(self.resource_prefix + self.target_uri))] + '-' + self.target_uri, regex_pattern=fr'{regex}', separator=self.service.GLUE.value['separator'], lowercase=True)}"
-
-    def build_glue_etl_compliant_name(self, regex) -> str:
-        return f"{slugify(self.resource_prefix + '-' + self.target_label[:(self.service.GLUE_ETL.value['max_length'] - len(self.resource_prefix + self.target_uri))] + '-' + self.target_uri, regex_pattern=fr'{regex}', separator=self.service.GLUE_ETL.value['separator'], lowercase=True)}"
-
-    def build_notebook_compliant_name(self, regex) -> str:
-        return f"{slugify(self.resource_prefix + '-' + self.target_label[:(self.service.NOTEBOOK.value['max_length'] - len(self.resource_prefix +self.target_uri))] + '-' + self.target_uri, regex_pattern=fr'{regex}', separator=self.service.NOTEBOOK.value['separator'], lowercase=True)}"
-
-    def build_opensearch_compliant_name(self, regex) -> str:
-        return f"{slugify(self.resource_prefix + '-' + self.target_label[:(self.service.OPENSEARCH.value['max_length'] - len(self.resource_prefix))], regex_pattern=fr'{regex}', separator=self.service.OPENSEARCH.value['separator'], lowercase=True)}"
-
-    def build_opensearch_serverless_compliant_name(self, regex) -> str:
-        return f"{slugify(self.resource_prefix + '-' + self.target_label[:(self.service.OPENSEARCH_SERVERLESS.value['max_length'] - len(self.resource_prefix))], regex_pattern=fr'{regex}', separator=self.service.OPENSEARCH_SERVERLESS.value['separator'], lowercase=True)}"
-
-    def build_default_compliant_name(self, regex) -> str:
-        return f"{slugify(self.resource_prefix + '-' + self.target_label[:(self.service.DEFAULT.value['max_length'] - len(self.resource_prefix +self.target_uri))] + '-' + self.target_uri, regex_pattern=fr'{regex}', separator=self.service.DEFAULT.value['separator'], lowercase=True)}"
+        regex = NamingConventionPattern[self.service].value['regex']
+        separator = NamingConventionPattern[self.service].value['separator']
+        max_length = NamingConventionPattern[self.service].value['max_length']
+        return f"{slugify(self.resource_prefix + '-' + self.target_label[:(max_length- len(self.resource_prefix + self.target_uri))] + '-' + self.target_uri, regex_pattern=fr'{regex}', separator=separator, lowercase=True)}"
