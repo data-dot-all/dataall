9e55407b6628601246bf65dc53e6d5ad5db94743
Noah Paige <noahpaig@amazon.com>
2023-09-29 12:44:31 -0400

    Try something

--- frontend/src/services/hooks/useClient.js
+++ frontend/src/services/hooks/useClient.js
@@ -51,11 +51,40 @@ export const useClient = () => {
         return forward(operation);
       });
       const errorLink = onError(({ graphQLErrors, networkError }) => {
+        // if (graphQLErrors) {
+        //   for (let err of graphQLErrors) {
+        //     switch (err.extensions.code) {
+        //       // Apollo Server sets code to UNAUTHENTICATED
+        //       // when an AuthenticationError is thrown in a resolver
+        //       case "UNAUTHENTICATED":
+        //         // Modify the operation context with a new token
+        //         const oldHeaders = operation.getContext().headers;
+        //         operation.setContext({
+        //           headers: {
+        //             ...oldHeaders,
+        //             authorization: getNewToken(),
+        //           },
+        //         });
+        //         // Retry the request, returning the new observable
+        //         return forward(operation);
+        //     }
+        //   }
+        // }
         if (graphQLErrors) {
           graphQLErrors.forEach(({ message, locations, path }) => {
             console.error(
               `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
             );
+            if (message === 'ReAuth Required') {
+              const oldHeaders = operation.getContext().headers;
+              operation.setContext({
+                headers: {
+                  ...oldHeaders,
+                  authorization: getNewToken(),
+                },
+              });
+              return forward(operation)
+            }
           });
         }
 
@@ -73,26 +102,51 @@ export const useClient = () => {
       setClient(apolloClient);
     };
     if (token) {
-      initClient().catch((e) => {
-        // IF COMING FROM RE AUTH
-        if (e.response.status === 401) {
-          reAuthInitiate();
-        } else {
-          console.error(e);
-        }
-      });
+      initClient().catch((e) => console.error(e));
     }
+    // if (token) {
+    //   initClient().catch((e) => {
+    //     // IF COMING FROM RE AUTH
+    //     if (e.response.status === 401) {
+    //       reAuthInitiate();
+    //     } else {
+    //       console.error(e);
+    //     }
+    //   });
+    // }
   }, [token, dispatch]);
   return client;
 };
 
 // Step-up - initiate
-export function reAuthInitiate() {
-  return () => {
-    // eslint-disable-next-line no-undef
-    console.error(`Re-Auth Required`);
-    return new Promise((resolve, reject) => {
-      useAuth();
-    });
-  };
+async function getNewToken() {
+  await Auth.signOut();
+  useAuth();
+  return useToken();
+  // const login = async () => {
+  //   Auth.federatedSignIn()
+  //     .then((user) => {
+  //       dispatch({
+  //         type: 'LOGIN',
+  //         payload: {
+  //           user: {
+  //             id: user.attributes.email,
+  //             email: user.attributes.email,
+  //             name: user.attributes.email
+  //           }
+  //         }
+  //       });
+  //     })
+  //     .catch((e) => {
+  //       console.error('Failed to authenticate user', e);
+  //     });
+  // };
+  // return () => {
+  //   // eslint-disable-next-line no-undef
+  //   console.error(`Re-Auth Required`);
+  //   return new Promise((resolve, reject) => {
+  //     useAuth();
+  //   });
+  // };
 }
+
