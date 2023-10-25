48c32e5a34e6ff535d5ffbae04eda474397dfb4a
Zilvinas Saltys <zilvinas.saltys@gmail.com>
2023-10-25 13:51:38 +0100

    feat: adding frontend and backend feature flags (#817)
    ### Feature or Bugfix
    - Feature
    
    ### Detail
    - Adding frontend support for all feature flags defined in config.json
    with a new util method isFeatureEnabled
    - Adding a new flag **preview_data** in the datasets module to control
    whether previewing data is allowed
    - Adding a new flag **glue_crawler** in the datasets module to control
    whether running glue crawler is allowed
    - Updating environment features to be hidden or visible based on whether
    the module is active. Adding a new util isAnyFeatureModuleEnabled to
    check whether to render the entire feature box.
    
    ### Relates
    N/A
    
    ### Security
    Not relevant
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache 2.0 license.
    
    ---------
    
    Co-authored-by: Zilvinas Saltys <zilvinas.saltys@yahooinc.com>

--- frontend/src/utils/helpers/moduleUtils.js
+++ frontend/src/utils/helpers/moduleUtils.js
@@ -33,6 +33,15 @@ function isModuleEnabled(module) {
   return getModuleActiveStatus(module);
 }
 
+function isAnyFeatureModuleEnabled() {
+  return !!(
+    isModuleEnabled(ModuleNames.PIPELINES) ||
+    isModuleEnabled(ModuleNames.DASHBOARDS) ||
+    isModuleEnabled(ModuleNames.MLSTUDIO) ||
+    isModuleEnabled(ModuleNames.NOTEBOOKS)
+  );
+}
+
 function getModuleActiveStatus(moduleKey) {
   if (
     config.modules &&
@@ -44,4 +53,26 @@ function getModuleActiveStatus(moduleKey) {
   return false;
 }
 
-export { ModuleNames, isModuleEnabled };
+function isFeatureEnabled(moduleKey, featureKey) {
+  if (
+    moduleKey === 'core' &&
+    config.core.features !== undefined &&
+    config.core.features[featureKey] !== undefined
+  ) {
+    return config.core.features[featureKey];
+  } else if (
+    getModuleActiveStatus(moduleKey) &&
+    config.modules[moduleKey]['features'] !== undefined &&
+    config.modules[moduleKey]['features'][featureKey] !== undefined
+  ) {
+    return config.modules[moduleKey]['features'][featureKey];
+  }
+  return false;
+}
+
+export {
+  ModuleNames,
+  isModuleEnabled,
+  isAnyFeatureModuleEnabled,
+  isFeatureEnabled
+};
