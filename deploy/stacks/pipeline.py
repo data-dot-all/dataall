be6eeb838928778494bbfd09e6dc7ee1472395c3
Arjun P <127312341+arjunyara@users.noreply.github.com>
2023-11-23 20:02:02 +0530

    Update pipeline.py
    corrected s3 cp command

--- deploy/stacks/pipeline.py
+++ deploy/stacks/pipeline.py
@@ -142,7 +142,7 @@ class PipelineStack(Stack):
                     'npm install -g aws-cdk',
                     f'aws codeartifact login --tool pip --repository {self.codeartifact.codeartifact_pip_repo_name} --domain {self.codeartifact.codeartifact_domain_name} --domain-owner {self.codeartifact.domain.attr_owner}',
                     'pip install -r deploy/requirements.txt',
-                    'aws s3 cp s3://s3://dpp-dp-data-all-configuration-bucket/cdk.context.json . --recursive',
+                    'aws s3 cp s3://dpp-dp-data-all-configuration-bucket/cdk.context.json . --recursive',
                     'cdk synth',
                     'echo ${CODEBUILD_SOURCE_VERSION}'
                 ],
