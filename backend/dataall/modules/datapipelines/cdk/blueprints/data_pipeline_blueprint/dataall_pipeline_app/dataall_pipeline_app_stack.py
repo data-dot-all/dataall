8ca73e6a144f2b9793068fb4644cf1e0bb80d679
Noah Paige <69586985+noah-paige@users.noreply.github.com>
2023-11-28 11:08:47 -0500

    Upgrade DDK and Resolve Data.all Pipelines (#866)
    ### Feature or Bugfix
    <!-- please choose -->
    - Enhancement
    
    ### Detail
    - Upgrade DDK Core library to v1.3.0
    - Remove calls to DDK CLI Library (deprecated)
    - Edit `blueprints/` to work with CDK-native App and use DDK
    Configurator
    
    Also as part of this PR:
    - Allowed the ability to update CDK Pipeline stacks (previously not
    supported)
    
    ### Relates
    - https://github.com/awslabs/aws-dataall/issues/747
    
    ### Security
    Please answer the questions below briefly where applicable, or write
    `N/A`. Based on
    [OWASP 10](https://owasp.org/Top10/en/).
    
    - Does this PR introduce or modify any input fields or queries - this
    includes
    fetching data from storage outside the application (e.g. a database, an
    S3 bucket)?
      - Is the input sanitized?
    - What precautions are you taking before deserializing the data you
    consume?
      - Is injection prevented by parametrizing queries?
      - Have you ensured no `eval` or similar functions are used?
    - Does this PR introduce any functionality or component that requires
    authorization?
    - How have you ensured it respects the existing AuthN/AuthZ mechanisms?
      - Are you logging failed auth attempts?
    - Are you using or adding any cryptographic features?
      - Do you use a standard proven implementations?
      - Are the used keys controlled by the customer? Where are they stored?
    - Are you introducing any new policies/roles/users?
      - Have you used the least-privilege principle? How?
    
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache 2.0 license.

---
+++ backend/dataall/modules/datapipelines/cdk/blueprints/data_pipeline_blueprint/dataall_pipeline_app/dataall_pipeline_app_stack.py
@@ -0,0 +1,26 @@
+from typing import Any, Optional
+
+from aws_cdk import Environment, Tags
+from aws_ddk_core import BaseStack, Configurator
+from constructs import Construct
+
+
+class DataallPipelineStack(BaseStack):
+    def __init__(
+        self, 
+        scope: Construct,
+        id: str,
+        environment_id: str,
+        env: Optional[Environment] = None,
+        **kwargs: Any
+    ) -> None:
+        super().__init__(
+            scope,
+            id,
+            environment_id=environment_id,
+            env=env or Configurator.get_environment(config_path="./ddk.json", environment_id=environment_id),
+            **kwargs
+        )
+        Configurator(scope=self, config="./ddk.json", environment_id=environment_id)
+
+        # The code that defines your stack goes here:
