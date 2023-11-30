6affeff26592c30ebf8ade2f49b91f12f1344196
dlpzx <dlpzx@amazon.com>
2023-11-30 10:39:10 +0100

    Add SCP error handling in Quicksight identity region checks

--- backend/dataall/base/aws/quicksight.py
+++ backend/dataall/base/aws/quicksight.py
@@ -10,6 +10,23 @@ logger.setLevel(logging.DEBUG)
 class QuicksightClient:
 
     DEFAULT_GROUP_NAME = 'dataall'
+    QUICKSIGHT_IDENTITY_REGIONS = [
+        {"name": 'US East (N. Virginia)', "code": 'us-east-1'},
+        {"name": 'US East (Ohio)', "code": 'us-east-2'},
+        {"name": 'US West (Oregon)', "code": 'us-west-2'},
+        {"name": 'Europe (Frankfurt)', "code": 'eu-central-1'},
+        {"name": 'Europe (Stockholm)', "code": 'eu-north-1'},
+        {"name": 'Europe (Ireland)', "code": 'eu-west-1'},
+        {"name": 'Europe (London)', "code": 'eu-west-2'},
+        {"name": 'Europe (Paris)', "code": 'eu-west-3'},
+        {"name": 'Asia Pacific (Singapore)', "code": 'ap-southeast-1'},
+        {"name": 'Asia Pacific (Sydney)', "code": 'ap-southeast-2'},
+        {"name": 'Asia Pacific (Tokyo)', "code": 'ap-northeast-1'},
+        {"name": 'Asia Pacific (Seoul)', "code": 'ap-northeast-2'},
+        {"name": 'South America (São Paulo)', "code": 'sa-east-1'},
+        {"name": 'Canada (Central)', "code": 'ca-central-1'},
+        {"name": 'Asia Pacific (Mumbai)', "code": 'ap-south-1'},
+    ]
 
     def __init__(self):
         pass
@@ -37,20 +54,31 @@ class QuicksightClient:
             the region quicksight uses as identity region
         """
         identity_region_rex = re.compile('Please use the (?P<region>.*) endpoint.')
-        identity_region = 'us-east-1'
-        client = QuicksightClient.get_quicksight_client(AwsAccountId=AwsAccountId, region=identity_region)
-        try:
-            response = client.describe_group(
-                AwsAccountId=AwsAccountId, GroupName=QuicksightClient.DEFAULT_GROUP_NAME, Namespace='default'
-            )
-        except client.exceptions.AccessDeniedException as e:
-            match = identity_region_rex.findall(str(e))
-            if match:
-                identity_region = match[0]
-            else:
-                raise e
-        except client.exceptions.ResourceNotFoundException:
-            pass
+        scp = 'with an explicit deny in a service control policy'
+        index = 0
+        while index < len(QuicksightClient.QUICKSIGHT_IDENTITY_REGIONS):
+            try:
+                identity_region = QuicksightClient.QUICKSIGHT_IDENTITY_REGIONS[index].get("code")
+                index += 1
+                client = QuicksightClient.get_quicksight_client(AwsAccountId=AwsAccountId, region=identity_region)
+                response = client.describe_group(
+                    AwsAccountId=AwsAccountId, GroupName=QuicksightClient.DEFAULT_GROUP_NAME, Namespace='default'
+                )
+                break
+            except client.exceptions.AccessDeniedException as e:
+                if scp in str(e):
+                    logger.info(f'Quicksight SCP found in {identity_region} for account {AwsAccountId}. Trying next region...')
+                else:
+                    logger.info(f'Quicksight identity region is not {identity_region}, selecting correct region endpoint...')
+                    match = identity_region_rex.findall(str(e))
+                    if match:
+                        identity_region = match[0]
+                        break
+                    else:
+                        raise e
+            except client.exceptions.ResourceNotFoundException:
+                pass
+        logger.info(f'Returning identity region = {identity_region} for account {AwsAccountId}')
         return identity_region
 
     @staticmethod
