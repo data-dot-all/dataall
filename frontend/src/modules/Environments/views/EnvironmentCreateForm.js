3208597750d9e4025e677a6337db45f759e87518
Noah Paige <noahpaig@amazon.com>
2023-12-20 12:03:00 -0500

    test custom bootstrap

--- frontend/src/modules/Environments/views/EnvironmentCreateForm.js
+++ frontend/src/modules/Environments/views/EnvironmentCreateForm.js
@@ -396,7 +396,7 @@ const EnvironmentCreateForm = (props) => {
                               <CopyToClipboard
                                 onCopy={() => copyNotification()}
                                 text={`aws cloudformation --region REGION create-stack --stack-name DataAllCustomCDKExecPolicyStack --template-body file://cdkExecPolicy.yaml --parameters ParameterKey=EnvironmentResourcePrefix,ParameterValue=dataall --capabilities CAPABILITY_NAMED_IAM && aws cloudformation wait stack-create-complete --stack-name DataAllCustomCDKExecPolicyStack --region REGION && cdk bootstrap --trust ${trustedAccount} -c @aws-cdk/core:newStyleStackSynthesis=true --cloudformation-execution-policies arn:aws:iam::ACCOUNT_ID:policy/DataAllCustomCDKPolicy aws://ACCOUNT_ID/REGION`}
-                                >
+                              >
                                 <IconButton>
                                   <CopyAllOutlined
                                     sx={{
