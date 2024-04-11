f8f892039e81d3abd237924ca2e0c5c9d60fb23b
Sofia Sazonova <sazonova@amazon.co.uk>
2024-04-11 13:30:17 +0100

    fix migration script

--- backend/migrations/versions/9dcc2f2b8858_add_organization_group_permissions.py
+++ backend/migrations/versions/9dcc2f2b8858_add_organization_group_permissions.py
@@ -9,6 +9,8 @@ Create Date: 2024-04-11 10:34:06.827154
 from alembic import op
 from sqlalchemy import orm
 from dataall.core.organizations.db.organization_models import OrganizationGroup, Organization
+from dataall.core.permissions.api.enums import PermissionType
+from dataall.core.permissions.services.permission_service import PermissionService
 from dataall.core.permissions.services.resource_policy_service import ResourcePolicyService
 from dataall.core.permissions.services.organization_permissions import (
     INVITE_ORGANIZATION_GROUP,
@@ -30,6 +32,19 @@ def get_session():
 
 def upgrade():
     session = get_session()
+    print(f"Adding organization group permissions...")
+    invite_permission = PermissionService.save_permission(
+        session=session,
+        name=INVITE_ORGANIZATION_GROUP,
+        description='INVITE_ORGANIZATION_GROUP',
+        permission_type=PermissionType.RESOURCE.name,
+    )
+    remove_permission = PermissionService.save_permission(
+        session=session,
+        name=REMOVE_ORGANIZATION_GROUP,
+        description='REMOVE_ORGANIZATION_GROUP',
+        permission_type=PermissionType.RESOURCE.name,
+    )
     all_org_groups = session.query(OrganizationGroup).all()
     for org_group in all_org_groups:
         ResourcePolicyService.attach_resource_policy(
@@ -43,4 +58,4 @@ def upgrade():
 
 def downgrade():
     # No downgrade. It's a bugfix
-    pass
+    print(f"Downgrade not supported for this migration: add_organization_group_permissions.")
