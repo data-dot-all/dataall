7da0c3d248595d0f846014f609dbb814039f77ff
Petros Kalos <kalosp@amazon.com>
2024-04-16 13:45:13 +0300

    configure logging level

--- backend/saveperms_handler.py
+++ backend/saveperms_handler.py
@@ -2,14 +2,18 @@
 The handler of this module will be called once upon every deployment
 """
 
+import logging
 import os
 
 from dataall.base.db import get_engine
 from dataall.base.loader import load_modules, ImportMode
 from dataall.core.permissions.services.tenant_policy_service import TenantPolicyService
 
+logger = logging.getLogger()
+logger.setLevel(os.environ.get('LOG_LEVEL', 'INFO'))
 
-def handler(event, context):
+
+def handler(event, context) -> None:
     load_modules(modes={ImportMode.API})
     envname = os.getenv('envname', 'local')
     engine = get_engine(envname=envname)
