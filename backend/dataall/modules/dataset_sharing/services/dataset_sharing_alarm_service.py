e691b0e2b63dd366644360364b629b5b1c66932a
dlpzx <71252798+dlpzx@users.noreply.github.com>
2024-04-26 13:13:11 +0200

    Refactor: uncouple datasets and dataset_sharing modules - part 1 (#1184)
    ### Feature or Bugfix
    - Refactoring
    
    ### Detail
    - Split the DatasetAlarm service into the alarms related to datasets and
    those related to sharing. This is needed as explained in full PR #1179
    
    ### Relates
    - #1179
    
    ### Security
    Please answer the questions below briefly where applicable, or write
    `N/A`. Based on
    [OWASP 10](https://owasp.org/Top10/en/). `N/A`
    
    - Does this PR introduce or modify any input fields or queries - this
    includes
    fetching data from storage outside the application (e.g. a database, an
    S3 bucket)?
      - Is the input sanitized?
    - What precautions are you taking before deserializing the data you
    consume?
      - Is injection prevented by parametrizing queries?
      - Have you ensured no `eval` or similar functions are used?
    - Does this PR introduce any functionality or component that requires
    authorization?
    - How have you ensured it respects the existing AuthN/AuthZ mechanisms?
      - Are you logging failed auth attempts?
    - Are you using or adding any cryptographic features?
      - Do you use a standard proven implementations?
      - Are the used keys controlled by the customer? Where are they stored?
    - Are you introducing any new policies/roles/users?
      - Have you used the least-privilege principle? How?
    
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache 2.0 license.

--- backend/dataall/modules/dataset_sharing/services/dataset_sharing_alarm_service.py
+++ backend/dataall/modules/dataset_sharing/services/dataset_sharing_alarm_service.py
@@ -3,13 +3,13 @@ from datetime import datetime
 
 from dataall.core.environment.db.environment_models import Environment
 from dataall.modules.dataset_sharing.db.share_object_models import ShareObject
-from dataall.modules.datasets_base.db.dataset_models import DatasetTable, Dataset, DatasetStorageLocation, DatasetBucket
+from dataall.modules.datasets_base.db.dataset_models import DatasetTable, DatasetStorageLocation, DatasetBucket
 from dataall.base.utils.alarm_service import AlarmService
 
 log = logging.getLogger(__name__)
 
 
-class DatasetAlarmService(AlarmService):
+class DatasetSharingAlarmService(AlarmService):
     """Contains set of alarms for datasets"""
 
     def trigger_table_sharing_failure_alarm(
@@ -69,24 +69,6 @@ class DatasetAlarmService(AlarmService):
         - AWS Account:                {target_environment.AwsAccountId}
         - Region:                            {target_environment.region}
         - Glue Database:              {table.GlueDatabaseName}shared
-    """
-        return self.publish_message_to_alarms_topic(subject, message)
-
-    def trigger_dataset_sync_failure_alarm(self, dataset: Dataset, error: str):
-        log.info(f'Triggering dataset {dataset.name} tables sync failure alarm...')
-        subject = f'Data.all Dataset Tables Sync Failure for {dataset.name}'[:100]
-        message = f"""
-You are receiving this email because your Data.all {self.envname} environment in the {self.region} region has entered the ALARM state, because it failed to synchronize Dataset {dataset.name} tables from AWS Glue to the Search Catalog.
-
-Alarm Details:
-    - State Change:               	OK -> ALARM
-    - Reason for State Change:      {error}
-    - Timestamp:                              {datetime.now()}
-    Dataset
-     - Dataset URI:                   {dataset.datasetUri}
-     - AWS Account:                {dataset.AwsAccountId}
-     - Region:                            {dataset.region}
-     - Glue Database:              {dataset.GlueDatabaseName}
     """
         return self.publish_message_to_alarms_topic(subject, message)
 
