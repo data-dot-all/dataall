ed7cc3eb904ff4562044d226d74643d39cb934de
Noah Paige <69586985+noah-paige@users.noreply.github.com>
2024-05-06 09:32:30 -0400

    Add order_by for paginated queries  (#1249)
    ### Feature or Bugfix
    <!-- please choose -->
    - Bugfix
    
    ### Detail
    - This PR aims to solve the following
    
    - (1) for particular queries (identified as ones that perform
    `.outerjoin()` operations and have results paginated with `paginate()`
    function - sometimes the returned query results is *less than* the limit
    set by the pageSize of the paginate function even when the total count
    is greater than the pageSize
    - Ex 1: 11 envs total, `query_user_environments()` returning 9 envs on
    1st page + 2 on 2nd page
    - Ex 2: 10 envs total, `query_user_environments()` returning 9 envs on
    1st page + no 2nd page
    
    - Believe this is to be happening due to the way SQLAlchemy is
    "uniquing" the records resulted from an outerjoin and then returning
    that result back to the frontend
    
    - Adding a `.distinct()` check on the query ensures each distinct record
    is returned (tested successfully)
    
    - (2) Currently we often times do not implement an `.order_by()`
    condition for the query used in `paginate()` and do not have a stable
    way of preserving order of the items returned from a query (i.e. when
    navigating through pages of response)
    - A generally good practice seems to include an `order_by()` on a column
    or set of columns
    - For each query used in `paginate()` this PR adds an `order_by()`
    condition (full list in comments below)
    
    Can read a bit more context from related issue linked below
    
    ### Relates
    - https://github.com/data-dot-all/dataall/issues/1241
    
    ### Security
    Please answer the questions below briefly where applicable, or write
    `N/A`. Based on
    [OWASP 10](https://owasp.org/Top10/en/).
    
    - Does this PR introduce or modify any input fields or queries - this
    includes
    fetching data from storage outside the application (e.g. a database, an
    S3 bucket)?
      - Is the input sanitized?
    - What precautions are you taking before deserializing the data you
    consume?
      - Is injection prevented by parametrizing queries?
      - Have you ensured no `eval` or similar functions are used?
    - Does this PR introduce any functionality or component that requires
    authorization?
    - How have you ensured it respects the existing AuthN/AuthZ mechanisms?
      - Are you logging failed auth attempts?
    - Are you using or adding any cryptographic features?
      - Do you use a standard proven implementations?
      - Are the used keys controlled by the customer? Where are they stored?
    - Are you introducing any new policies/roles/users?
      - Have you used the least-privilege principle? How?
    
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache 2.0 license.

--- backend/dataall/modules/dataset_sharing/db/share_object_repositories.py
+++ backend/dataall/modules/dataset_sharing/db/share_object_repositories.py
@@ -594,7 +594,9 @@ class ShareObjectRepository:
                     else query.filter(shareable_objects.c.healthStatus != ShareItemHealthStatus.Healthy.value)
                 )
 
-        return paginate(query, data.get('page', 1), data.get('pageSize', 10)).to_dict()
+        return paginate(
+            query.order_by(shareable_objects.c.itemName).distinct(), data.get('page', 1), data.get('pageSize', 10)
+        ).to_dict()
 
     @staticmethod
     def list_user_received_share_requests(session, username, groups, data=None):
@@ -629,7 +631,7 @@ class ShareObjectRepository:
         if data and data.get('share_iam_roles'):
             if len(data.get('share_iam_roles')) > 0:
                 query = query.filter(ShareObject.principalIAMRoleName.in_(data.get('share_iam_roles')))
-        return paginate(query, data.get('page', 1), data.get('pageSize', 10)).to_dict()
+        return paginate(query.order_by(ShareObject.shareUri), data.get('page', 1), data.get('pageSize', 10)).to_dict()
 
     @staticmethod
     def list_user_sent_share_requests(session, username, groups, data=None):
@@ -668,7 +670,7 @@ class ShareObjectRepository:
         if data and data.get('share_iam_roles'):
             if len(data.get('share_iam_roles')) > 0:
                 query = query.filter(ShareObject.principalIAMRoleName.in_(data.get('share_iam_roles')))
-        return paginate(query, data.get('page', 1), data.get('pageSize', 10)).to_dict()
+        return paginate(query.order_by(ShareObject.shareUri), data.get('page', 1), data.get('pageSize', 10)).to_dict()
 
     @staticmethod
     def get_share_by_dataset_and_environment(session, dataset_uri, environment_uri):
@@ -1001,11 +1003,15 @@ class ShareObjectRepository:
 
     @staticmethod
     def query_dataset_shares(session, dataset_uri) -> Query:
-        return session.query(ShareObject).filter(
-            and_(
-                ShareObject.datasetUri == dataset_uri,
-                ShareObject.deleted.is_(None),
+        return (
+            session.query(ShareObject)
+            .filter(
+                and_(
+                    ShareObject.datasetUri == dataset_uri,
+                    ShareObject.deleted.is_(None),
+                )
             )
+            .order_by(ShareObject.shareUri)
         )
 
     @staticmethod
@@ -1157,7 +1163,9 @@ class ShareObjectRepository:
 
         if data.get('uniqueShares', False):
             q = q.filter(ShareObject.principalType != PrincipalType.ConsumptionRole.value)
-            q = q.distinct(ShareObject.shareUri)
+            q = q.order_by(ShareObject.shareUri).distinct(ShareObject.shareUri)
+        else:
+            q = q.order_by(ShareObjectItem.itemName).distinct()
 
         if data.get('term'):
             term = data.get('term')
