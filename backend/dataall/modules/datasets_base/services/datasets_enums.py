81de06df20b0d255d79eadb47174def55d78db4f
dlpzx <dlpzx@amazon.com>
2024-05-07 08:56:12 +0200

    Add generic dataset db model

--- backend/dataall/modules/datasets_base/services/datasets_enums.py
+++ backend/dataall/modules/datasets_base/services/datasets_enums.py
@@ -4,6 +4,8 @@ from dataall.base.db.exceptions import InvalidInput
 
 custom_confidentiality_mapping = config.get_property('modules.s3_datasets.features.custom_confidentiality_mapping', {})
 
+class DatasetType(GraphQLEnumMapper):
+    S3 = 'S3'
 
 class DatasetRole(GraphQLEnumMapper):
     # Permissions on a dataset
