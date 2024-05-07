41db8edd2ddfc6156c46cb05b3511b5cb2b5145e
dlpzx <dlpzx@amazon.com>
2024-05-07 15:22:50 +0200

    Fix migration references to Dataset and add new migration script with creation renamed table s3_dataset

--- backend/migrations/versions/458572580709_remove_dataset_table_read_permissions_from_env_admins.py
+++ backend/migrations/versions/458572580709_remove_dataset_table_read_permissions_from_env_admins.py
@@ -7,15 +7,20 @@ Create Date: 2024-05-01 17:14:08.190904
 """
 
 from alembic import op
-from sqlalchemy import orm
 from sqlalchemy import and_
+from sqlalchemy import orm, Column, String, Boolean
+from sqlalchemy.dialects import postgresql
+from sqlalchemy.ext.declarative import declarative_base
+
+from dataall.base.db import utils, Resource
 
 from dataall.core.environment.services.environment_service import EnvironmentService
 from dataall.core.permissions.services.resource_policy_service import ResourcePolicyService
 
-from dataall.modules.s3_datasets.db.dataset_models import DatasetTable, S3Dataset
+from dataall.modules.s3_datasets.db.dataset_models import DatasetTable
 from dataall.modules.dataset_sharing.db.share_object_models import ShareObject, ShareObjectItem
 from dataall.modules.dataset_sharing.services.dataset_sharing_enums import ShareItemStatus, ShareableType, PrincipalType
+from dataall.modules.datasets_base.services.datasets_enums import ConfidentialityClassification, Language
 
 # revision identifiers, used by Alembic.
 revision = '458572580709'
@@ -30,10 +35,57 @@ def get_session():
     return session
 
 
+Base = declarative_base()
+
+
+class Dataset(Resource, Base):
+    __tablename__ = 'dataset'
+    environmentUri = Column(String, nullable=False)
+    organizationUri = Column(String, nullable=False)
+    datasetUri = Column(String, primary_key=True, default=utils.uuid('dataset'))
+    region = Column(String, default='eu-west-1')
+    AwsAccountId = Column(String, nullable=False)
+    S3BucketName = Column(String, nullable=False)
+    GlueDatabaseName = Column(String, nullable=False)
+    GlueProfilingJobName = Column(String)
+    GlueProfilingTriggerSchedule = Column(String)
+    GlueProfilingTriggerName = Column(String)
+    GlueDataQualityJobName = Column(String)
+    GlueDataQualitySchedule = Column(String)
+    GlueDataQualityTriggerName = Column(String)
+    IAMDatasetAdminRoleArn = Column(String, nullable=False)
+    IAMDatasetAdminUserArn = Column(String, nullable=False)
+    KmsAlias = Column(String, nullable=False)
+    language = Column(String, nullable=False, default=Language.English.value)
+    topics = Column(postgresql.ARRAY(String), nullable=True)
+    confidentiality = Column(String, nullable=False, default=ConfidentialityClassification.Unclassified.value)
+    tags = Column(postgresql.ARRAY(String))
+
+    bucketCreated = Column(Boolean, default=False)
+    glueDatabaseCreated = Column(Boolean, default=False)
+    iamAdminRoleCreated = Column(Boolean, default=False)
+    iamAdminUserCreated = Column(Boolean, default=False)
+    kmsAliasCreated = Column(Boolean, default=False)
+    lakeformationLocationCreated = Column(Boolean, default=False)
+    bucketPolicyCreated = Column(Boolean, default=False)
+
+    businessOwnerEmail = Column(String, nullable=True)
+    businessOwnerDelegationEmails = Column(postgresql.ARRAY(String), nullable=True)
+    stewards = Column(String, nullable=True)
+
+    SamlAdminGroupName = Column(String, nullable=True)
+
+    importedS3Bucket = Column(Boolean, default=False)
+    importedGlueDatabase = Column(Boolean, default=False)
+    importedKmsKey = Column(Boolean, default=False)
+    importedAdminRole = Column(Boolean, default=False)
+    imported = Column(Boolean, default=False)
+
+
 def upgrade():
     session = get_session()
 
-    datasets: [S3Dataset] = session.query(S3Dataset).all()
+    datasets: [Dataset] = session.query(Dataset).all()
     for dataset in datasets:
         environment = EnvironmentService.get_environment_by_uri(session, uri=dataset.environmentUri)
         env_admin_group = environment.SamlGroupName
---
+++ backend/migrations/versions/d059eead99c2_rename_dataset_table_as_s3_dataset.py
@@ -0,0 +1,42 @@
+"""rename_dataset_table_as_s3_dataset
+
+Revision ID: d059eead99c2
+Revises: 458572580709
+Create Date: 2024-05-07 15:01:14.241572
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd059eead99c2'
+down_revision = '458572580709'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.rename_table('dataset', 's3_dataset')
+    op.execute('ALTER INDEX dataset_pkey RENAME TO s3_dataset_pkey')
+    op.drop_constraint(constraint_name="dataset_environmentUri_fkey", table_name="s3_dataset", type_="foreignkey")
+    op.create_foreign_key(
+        constraint_name="s3_dataset_environmentUri_fkey",
+        source_table="s3_dataset",
+        referent_table="environment",
+        local_cols=["environmentUri"],
+        remote_cols=["environmentUri"])
+
+
+def downgrade():
+    op.rename_table('s3_dataset', 'dataset')
+    op.execute('ALTER INDEX s3_dataset_pkey RENAME TO dataset_pkey')
+    op.drop_constraint(constraint_name="s3_dataset_environmentUri_fkey", table_name="dataset", type_="foreignkey")
+    op.create_foreign_key(
+        constraint_name="dataset_environmentUri_fkey",
+        source_table="dataset",
+        referent_table="environment",
+        local_cols=["environmentUri"],
+        remote_cols=["environmentUri"])
+
+
