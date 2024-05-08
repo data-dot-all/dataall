02abe68c9ba386d9d032d58a3e783c6e144fa550
Tejas Rajopadhye <rajopadhye.tejas@gmail.com>
2024-05-08 16:29:42 -0500

    Resolving issue with alembic

--- backend/migrations/versions/b833ad41db68_maintenance_window_schema.py
+++ backend/migrations/versions/b833ad41db68_maintenance_window_schema.py
@@ -16,7 +16,7 @@ from dataall.base.db import get_engine, has_table, Base
 
 # revision identifiers, used by Alembic.
 revision = 'b833ad41db68'
-down_revision = 'c6d01930179d'
+down_revision = '458572580709'
 branch_labels = None
 depends_on = None
 
