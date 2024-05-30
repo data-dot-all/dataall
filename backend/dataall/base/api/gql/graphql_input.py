a8d3da01079c3a3abc17f3c2aad6201054782636
Noah Paige <noahpaig@amazon.com>
2024-05-30 16:09:57 -0400

    Allow descriptions parameter for graphql fields and input args

--- backend/dataall/base/api/gql/graphql_input.py
+++ backend/dataall/base/api/gql/graphql_input.py
@@ -2,15 +2,19 @@ import textwrap
 
 from ._cache import cache_instances
 
-
 @cache_instances
 class InputType:
-    def __init__(self, name, arguments):
+    def __init__(self, name, arguments, description=''):
         self.name = name
         self.arguments = arguments
+        self.description = description
 
     def gql(self):
-        n = '\n'
+        description_str = ''
+        n = "\n"
+        if self.description:
+            description_str = f" \"\"\"{self.description}\"\"\" {n}"
+
         # args = f"{', '.join([arg.name+':'+ arg.type.gql() for arg in self.arguments])}"
         args = f"{', '.join([arg.gql() for arg in self.arguments])}"
-        return '\n'.join(textwrap.wrap(f'input {self.name}{{{n} {args} }}'))
+        return description_str + '\n'.join(textwrap.wrap(f'input {self.name}{{{n} {args} }}'))
