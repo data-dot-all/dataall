f6d0703d8effb3c918249431f6cf0f9a9433cbdb
Sofia Sazonova <sazonova@amazon.co.uk>
2024-06-20 15:53:18 +0100

    fix handler + logs

--- backend/deployment_triggers/dataall_migrate_handler.py
+++ backend/deployment_triggers/dataall_migrate_handler.py
@@ -1,6 +1,12 @@
+import logging
+import os
+
 from migrations.dataall_migrations.herder import Herder
 
+logger = logging.getLogger()
+logger.setLevel(os.environ.get('LOG_LEVEL', 'INFO'))
 
-def handler():
+
+def handler(event, context) -> None:
     H = Herder()
     H.upgrade()
