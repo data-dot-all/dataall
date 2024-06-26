90835fbb951fe027c03470e7749532ae702c7211
Anushka Singh <anushka.singh.2511@gmail.com>
2024-06-26 11:17:22 -0400

    Issue1248: Persistent Email Reminders (#1354)
    ### Feature or Bugfix
    - Feature
    
    
    ### Detail
    - When a share request is initiated and remains pending for an extended
    period, dataset producers will receive automated email reminders at
    predefined intervals. These reminders will prompt producers to either
    approve or extend the share request, thereby preventing delays in
    accessing datasets.
    
    Attaching screenshots for emails:
    
    <img width="1336" alt="Screenshot 2024-06-20 at 5 34 31 PM"
    src="https://github.com/data-dot-all/dataall/assets/26413731/d7be28c3-5c98-4146-92b1-295e136137a3">
    
    <img width="1322" alt="Screenshot 2024-06-20 at 5 34 52 PM"
    src="https://github.com/data-dot-all/dataall/assets/26413731/047556e8-59ee-4ebf-b8a7-c0a6684e2a63">
    
    
    - Email will be sent every Monday at 9am UTC. Schedule can be changed in
    cron expression in container.py
    
    ### Relates
    - https://github.com/data-dot-all/dataall/issues/1248
    
    ### Security
    Please answer the questions below briefly where applicable, or write
    `N/A`. Based on
    [OWASP 10](https://owasp.org/Top10/en/).
    
    - Does this PR introduce or modify any input fields or queries - this
    includes
    fetching data from storage outside the application (e.g. a database, an
    S3 bucket)?
      - Is the input sanitized?
    - What precautions are you taking before deserializing the data you
    consume?
      - Is injection prevented by parametrizing queries?
      - Have you ensured no `eval` or similar functions are used?
    - Does this PR introduce any functionality or component that requires
    authorization?
    - How have you ensured it respects the existing AuthN/AuthZ mechanisms?
      - Are you logging failed auth attempts?
    - Are you using or adding any cryptographic features?
      - Do you use a standard proven implementations?
      - Are the used keys controlled by the customer? Where are they stored?
    - Are you introducing any new policies/roles/users?
      - Have you used the least-privilege principle? How?
    
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache 2.0 license.
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: Anushka Singh <anushka.singh@yahooinc.com>
    Co-authored-by: trajopadhye <tejas.rajopadhye@yahooinc.com>
    Co-authored-by: Mohit Arora <marora@yahooinc.com>
    Co-authored-by: rbernota <rbernota@yahooinc.com>
    Co-authored-by: Rick Bernotas <rbernota@verizonmedia.com>
    Co-authored-by: Raj Chopde <rchopde@yahooinc.com>
    Co-authored-by: Noah Paige <69586985+noah-paige@users.noreply.github.com>
    Co-authored-by: dlpzx <71252798+dlpzx@users.noreply.github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: jaidisido <jaidisido@gmail.com>
    Co-authored-by: dlpzx <dlpzx@amazon.com>
    Co-authored-by: mourya-33 <134511711+mourya-33@users.noreply.github.com>
    Co-authored-by: nikpodsh <124577300+nikpodsh@users.noreply.github.com>
    Co-authored-by: MK <manjula_kasturi@hotmail.com>
    Co-authored-by: Manjula <manjula.kasturi@gmail.com>
    Co-authored-by: Zilvinas Saltys <zilvinas.saltys@gmail.com>
    Co-authored-by: Zilvinas Saltys <zilvinas.saltys@yahooinc.com>
    Co-authored-by: Daniel Lorch <98748454+lorchda@users.noreply.github.com>
    Co-authored-by: Tejas Rajopadhye <71188245+TejasRGitHub@users.noreply.github.com>
    Co-authored-by: Zilvinas Saltys <zilvinas.saltys@oath.com>
    Co-authored-by: Sofia Sazonova <sofia-s@304.ru>
    Co-authored-by: Sofia Sazonova <sazonova@amazon.co.uk>

--- backend/dataall/modules/shares_base/services/share_notification_service.py
+++ backend/dataall/modules/shares_base/services/share_notification_service.py
@@ -9,6 +9,7 @@ from dataall.modules.shares_base.db.share_object_models import ShareObject
 from dataall.base.context import get_context
 from dataall.modules.shares_base.services.shares_enums import ShareObjectStatus
 from dataall.modules.notifications.db.notification_repositories import NotificationRepository
+from dataall.modules.notifications.services.ses_email_notification_service import SESEmailNotificationService
 from dataall.modules.datasets_base.db.dataset_models import DatasetBase
 
 log = logging.getLogger(__name__)
@@ -56,11 +57,47 @@ class ShareNotificationService:
         self._create_notification_task(subject=subject, msg=email_notification_msg)
         return notifications
 
+    def notify_persistent_email_reminder(self, email_id: str):
+        share_link_text = ''
+        if os.environ.get('frontend_domain_url'):
+            share_link_text = (
+                f'<br><br>Please visit data.all <a href="{os.environ.get("frontend_domain_url")}'
+                f'/console/shares/{self.share.shareUri}">share link</a> '
+                f'to review and take appropriate action or view more details.'
+            )
+
+        msg_intro = f"""Dear User,
+        This is a reminder that a share request for the dataset "{self.dataset.label}" submitted by {email_id} 
+        on behalf of principal "{self.share.principalId}" is still pending and has not been addressed.
+        """
+
+        msg_end = """Your prompt attention to this matter is greatly appreciated.
+        Best regards,
+        The Data.all Team
+        """
+
+        subject = f'URGENT REMINDER: Data.all | Action Required on Pending Share Request for {self.dataset.label}'
+        email_notification_msg = msg_intro + share_link_text + msg_end
+
+        notifications = self.register_notifications(
+            notification_type=DataSharingNotificationType.SHARE_OBJECT_SUBMITTED.value, msg=msg_intro
+        )
+
+        self._create_persistent_reminder_notification_task(subject=subject, msg=email_notification_msg)
+        return notifications
+
     def notify_share_object_approval(self, email_id: str):
         share_link_text = ''
         if os.environ.get('frontend_domain_url'):
-            share_link_text = f'<br><br> Please visit data.all <a href="{os.environ.get("frontend_domain_url")}/console/shares/{self.share.shareUri}">share link </a> to take action or view more details'
-        msg = f'User {email_id} APPROVED share request for dataset {self.dataset.label} for principal {self.share.principalId}'
+            share_link_text = (
+                f'<br><br> Please visit data.all <a href="{os.environ.get("frontend_domain_url")}'
+                f'/console/shares/{self.share.shareUri}">share link </a> '
+                f'to take action or view more details'
+            )
+        msg = (
+            f'User {email_id} APPROVED share request for dataset {self.dataset.label} '
+            f'for principal {self.share.principalId}'
+        )
         subject = f'Data.all | Share Request Approved for {self.dataset.label}'
         email_notification_msg = msg + share_link_text
 
@@ -163,6 +200,45 @@ class ShareNotificationService:
                         self.session.commit()
 
                         Worker.queue(engine=get_context().db_engine, task_ids=[notification_task.taskUri])
+                else:
+                    log.info(f'Notification type : {share_notification_config_type} is not active')
+        else:
+            log.info('Notifications are not active')
+
+    def _create_persistent_reminder_notification_task(self, subject, msg):
+        """
+        At the moment just for notification_config_type = email, but designed for additional notification types
+        Emails sent to:
+            - dataset.SamlAdminGroupName
+            - dataset.stewards
+        """
+        share_notification_config = config.get_property(
+            'modules.datasets_base.features.share_notifications', default=None
+        )
+        if share_notification_config:
+            for share_notification_config_type in share_notification_config.keys():
+                n_config = share_notification_config[share_notification_config_type]
+                if n_config.get('active', False) == True:
+                    notification_recipient_groups_list = [self.dataset.SamlAdminGroupName, self.dataset.stewards]
+
+                    if share_notification_config_type == 'email':
+                        notification_task: Task = Task(
+                            action='notification.service',
+                            targetUri=self.share.shareUri,
+                            payload={
+                                'notificationType': share_notification_config_type,
+                                'subject': subject,
+                                'message': msg,
+                                'recipientGroupsList': notification_recipient_groups_list,
+                                'recipientEmailList': [],
+                            },
+                        )
+                        self.session.add(notification_task)
+                        self.session.commit()
+
+                        SESEmailNotificationService.send_email_task(
+                            subject, msg, notification_recipient_groups_list, []
+                        )
                 else:
                     log.info(f'Notification type : {share_notification_config_type} is not active')
         else:
