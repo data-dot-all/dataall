b5ce6ad2c35a48e337b73befb2f95ce9696d6861
Sofia Sazonova <sazonova@amazon.co.uk>
2024-06-27 10:56:02 +0100

    experiment for docker

--- backend/deployment_triggers/dataall_migrate_handler.py
+++ backend/deployment_triggers/dataall_migrate_handler.py
@@ -6,7 +6,25 @@ from migrations.dataall_migrations.herder import Herder
 logger = logging.getLogger()
 logger.setLevel(os.environ.get('LOG_LEVEL', 'INFO'))
 
+class BaseC:
+    name = "base"
+    @classmethod
+    def test(cls):
+        print(cls.name)
+
+
+class A(BaseC):
+    name = "class A"
+
+class B(BaseC):
+    name = "class B"
+    @classmethod
+    def test(cls):
+        print(cls.name)
+
 
 def handler(event, context) -> None:
-    H = Herder()
-    H.upgrade()
+    #H = Herder()
+    #H.upgrade()
+    A.test()
+    B.test()
--- backend/migrations/dataall_migrations/versions/remove_wildcard_share_policy.py
+++ backend/migrations/dataall_migrations/versions/remove_wildcard_share_policy.py
@@ -21,45 +21,46 @@ class RemoveWildCard(BaseDataAllMigration):
 
     @classmethod
     def up(cls):
+        print('removing wildcard from sharing policy')
         ENVNAME = os.environ.get('envname', 'local')
         ENGINE = get_engine(envname=ENVNAME)
-        # with ENGINE.scoped_session() as session:
-        #     all_envs = EnvironmentRepository.query_all_active_environments(session)
-        #     for env in all_envs:
-        #         cons_roles = EnvironmentRepository.query_all_environment_consumption_roles(
-        #             session, env.environmentUri, filter=None
-        #         )
-        #         for role in cons_roles:
-        #             share_policy_service = SharePolicyService(
-        #                 environmentUri=env.environmentUri,
-        #                 account=env.AwsAccountId,
-        #                 region=env.region,
-        #                 role_name=role.IAMRoleName,
-        #                 resource_prefix=env.resourcePrefix,
-        #             )
-        #             share_resource_policy_name = share_policy_service.generate_policy_name()
-        #             version_id, policy_document = IAM.get_managed_policy_default_version(
-        #                 env.AwsAccountId, env.region, policy_name=share_resource_policy_name
-        #             )
-        #             if policy_document is not None:
-        #                 statements = policy_document.get('Statement', [])
-        #                 for statement in statements:
-        #                     if statement['Sid'] in [
-        #                         f'{IAM_S3_BUCKETS_STATEMENT_SID}S3',
-        #                         f'{IAM_S3_ACCESS_POINTS_STATEMENT_SID}S3',
-        #                     ]:
-        #                         actions = set(statement['Actions'])
-        #                         if 's3:*' in actions:
-        #                             actions.remove('s3:*')
-        #                             actions.add('s3:List*')
-        #                             actions.add('s3:Describe*')
-        #                             actions.add('s3:GetObject')
-        #                         statement['Actions'] = list(actions)
-        #                 policy_document['Statement'] = statements
-        #                 IAM.update_managed_policy_default_version(
-        #                     env.AwsAccountId,
-        #                     env.region,
-        #                     share_resource_policy_name,
-        #                     version_id,
-        #                     json.dumps(policy_document),
-        #                 )
+        with ENGINE.scoped_session() as session:
+            all_envs = EnvironmentRepository.query_all_active_environments(session)
+            for env in all_envs:
+                cons_roles = EnvironmentRepository.query_all_environment_consumption_roles(
+                    session, env.environmentUri, filter=None
+                )
+                for role in cons_roles:
+                    share_policy_service = SharePolicyService(
+                        environmentUri=env.environmentUri,
+                        account=env.AwsAccountId,
+                        region=env.region,
+                        role_name=role.IAMRoleName,
+                        resource_prefix=env.resourcePrefix,
+                    )
+                    share_resource_policy_name = share_policy_service.generate_policy_name()
+                    version_id, policy_document = IAM.get_managed_policy_default_version(
+                        env.AwsAccountId, env.region, policy_name=share_resource_policy_name
+                    )
+                    if policy_document is not None:
+                        statements = policy_document.get('Statement', [])
+                        for statement in statements:
+                            if statement['Sid'] in [
+                                f'{IAM_S3_BUCKETS_STATEMENT_SID}S3',
+                                f'{IAM_S3_ACCESS_POINTS_STATEMENT_SID}S3',
+                            ]:
+                                actions = set(statement['Actions'])
+                                if 's3:*' in actions:
+                                    actions.remove('s3:*')
+                                    actions.add('s3:List*')
+                                    actions.add('s3:Describe*')
+                                    actions.add('s3:GetObject')
+                                statement['Actions'] = list(actions)
+                        policy_document['Statement'] = statements
+                        IAM.update_managed_policy_default_version(
+                            env.AwsAccountId,
+                            env.region,
+                            share_resource_policy_name,
+                            version_id,
+                            json.dumps(policy_document),
+                        )
