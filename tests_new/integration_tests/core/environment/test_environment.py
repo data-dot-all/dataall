c3c58bd5188128f62aba801941f6403a0c6bf0aa
Petros Kalos <kalosp@amazon.com>
2024-06-28 13:55:42 +0300

    add environment tests (#1371)
    ### Feature or Bugfix
    Feature
    
    ### Detail
    * add list_environment tests
    * add test for updating an environment (via update_stack)
    * generalise the polling functions for stacks
    
    ### Relates
    #1220 
    
    ### Security
    Please answer the questions below briefly where applicable, or write
    `N/A`. Based on
    [OWASP 10](https://owasp.org/Top10/en/).
    
    - Does this PR introduce or modify any input fields or queries - this
    includes
    fetching data from storage outside the application (e.g. a database, an
    S3 bucket)?
      - Is the input sanitized?
    - What precautions are you taking before deserializing the data you
    consume?
      - Is injection prevented by parametrizing queries?
      - Have you ensured no `eval` or similar functions are used?
    - Does this PR introduce any functionality or component that requires
    authorization?
    - How have you ensured it respects the existing AuthN/AuthZ mechanisms?
      - Are you logging failed auth attempts?
    - Are you using or adding any cryptographic features?
      - Do you use a standard proven implementations?
      - Are the used keys controlled by the customer? Where are they stored?
    - Are you introducing any new policies/roles/users?
      - Have you used the least-privilege principle? How?
    
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache 2.0 license.

--- tests_new/integration_tests/core/environment/test_environment.py
+++ tests_new/integration_tests/core/environment/test_environment.py
@@ -1,22 +1,27 @@
+import logging
 from datetime import datetime
 
 from assertpy import assert_that
 
-from integration_tests.core.environment.queries import update_environment, get_environment
+from integration_tests.core.environment.queries import get_environment, update_environment, list_environments
+from integration_tests.core.stack.queries import update_stack
+from integration_tests.core.stack.utils import check_stack_in_progress, check_stack_ready
 from integration_tests.errors import GqlError
+
+log = logging.getLogger(__name__)
 
 
 def test_create_env(session_env1):
-    assert_that(session_env1.stack.status).is_equal_to('CREATE_COMPLETE')
+    assert_that(session_env1.stack.status).is_in('CREATE_COMPLETE', 'UPDATE_COMPLETE')
 
 
 def test_modify_env(client1, session_env1):
     test_description = f'a test description {datetime.utcnow().isoformat()}'
     env_uri = session_env1.environmentUri
     updated_env = update_environment(client1, env_uri, {'description': test_description})
-    assert_that(updated_env).contains_entry({'environmentUri': env_uri}, {'description': test_description})
+    assert_that(updated_env).contains_entry(environmentUri=env_uri, description=test_description)
     env = get_environment(client1, env_uri)
-    assert_that(env).contains_entry({'environmentUri': env_uri}, {'description': test_description})
+    assert_that(env).contains_entry(environmentUri=env_uri, description=test_description)
 
 
 def test_modify_env_unauthorized(client1, client2, session_env1):
@@ -26,6 +31,24 @@ def test_modify_env_unauthorized(client1, client2, session_env1):
         client2, env_uri, {'description': test_description}
     ).contains('UnauthorizedOperation', env_uri)
     env = get_environment(client1, env_uri)
-    assert_that(env).contains_entry({'environmentUri': env_uri}).does_not_contain_entry(
-        {'description': test_description}
-    )
+    assert_that(env).contains_entry(environmentUri=env_uri).does_not_contain_entry(description=test_description)
+
+
+def test_list_envs_authorized(client1, session_env1, session_env2, session_id):
+    assert_that(list_environments(client1, term=session_id).nodes).is_length(2)
+
+
+def test_list_envs_unauthorized(client2, session_env1, session_env2, session_id):
+    assert_that(list_environments(client2, term=session_id).nodes).is_length(0)
+
+
+def test_persistent_env_update(client1, persistent_env1):
+    # wait for stack to get to a final state before triggering an update
+    stack_uri = persistent_env1.stack.stackUri
+    env_uri = persistent_env1.environmentUri
+    check_stack_ready(client1, env_uri, stack_uri)
+    update_stack(client1, env_uri, 'environment')
+    # wait for stack to move to "in_progress" state
+    check_stack_in_progress(client1, env_uri, stack_uri)
+    stack = check_stack_ready(client1, env_uri, stack_uri)
+    assert_that(stack.status).is_equal_to('UPDATE_COMPLETE')
