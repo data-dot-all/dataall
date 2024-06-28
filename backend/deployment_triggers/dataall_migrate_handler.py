cdf1f923bbb12ff5d144e5be979b7c550355ab47
Sofia Sazonova <sazonova@amazon.co.uk>
2024-06-28 10:55:10 +0100

    migration handler back to normal

--- backend/deployment_triggers/dataall_migrate_handler.py
+++ backend/deployment_triggers/dataall_migrate_handler.py
@@ -1,44 +1,13 @@
 import logging
 import os
-import platform
 from migrations.dataall_migrations.herder import Herder
 
 logger = logging.getLogger()
 logger.setLevel(os.environ.get('LOG_LEVEL', 'INFO'))
 
-class BaseC:
-    name = "base"
-    @classmethod
-    def test(cls):
-        print("test")
-        print("Classname: ", cls.__name__)
-        print("Attribute value: ", cls.name)
-
-        print("All attributes: ", cls.__dict__)
-
-
-class A(BaseC):
-    name = "class A"
-
-class B(BaseC):
-    name = "class B"
-    @classmethod
-    def test(cls):
-        print("test")
-        print("Classname: ", cls.__name__)
-        print("Attribute value: ", cls.name)
-
-        print("All attributes: ", cls.__dict__)
 
 def handler(event, context) -> None:
-    #H = Herder()
-    #H.upgrade()
-    print("Python version = ", platform.python_version())
-    print("Python compiler = ", platform.python_compiler())
-    print("Python release = ", platform.release())
-    print("Python platform = ", platform.platform())
+    H = Herder()
+    H.upgrade()
 
-    BaseC.test()
-    A.test()
-    B.test()
 
