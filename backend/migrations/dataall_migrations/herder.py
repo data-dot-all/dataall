c04d35f10bd54cdd221591fb904da45a0dc67c39
Sofia Sazonova <sazonova@amazon.co.uk>
2024-07-02 10:46:25 +0100

    Revert changes if ugrade|downgrade fails

--- backend/migrations/dataall_migrations/herder.py
+++ backend/migrations/dataall_migrations/herder.py
@@ -44,7 +44,7 @@ class Herder:
                 self.last_key = key
                 break
 
-    def upgrade(self, target_key=None, start_key=None, downgrade_if_fails = True):
+    def upgrade(self, target_key=None, start_key=None, downgrade_if_fails=True):
         if start_key is not None:
             self.current_key = self.migration_path[start_key].next()
             if self.current_key is None:
@@ -72,7 +72,7 @@ class Herder:
         logger.info('Upgrade completed')
         return True
 
-    def downgrade(self, target_key=None, start_key=None, upgrade_if_fails = True):
+    def downgrade(self, target_key=None, start_key=None, upgrade_if_fails=True):
         self.current_key = start_key if start_key is not None else self.last_key
         logger.info(
             f"Downgrade from {start_key if start_key is not None else 'latest'} to {target_key if target_key is not None else 'initial'}"
