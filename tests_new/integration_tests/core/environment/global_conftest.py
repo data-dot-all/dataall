c4cf34c185f4a4867e171f90c697ecc5b6e2dd5f
Noah Paige <noahpaig@amazon.com>
2024-07-08 15:54:09 -0400

    Fix sessio ncreds

--- tests_new/integration_tests/core/environment/global_conftest.py
+++ tests_new/integration_tests/core/environment/global_conftest.py
@@ -106,3 +106,45 @@ def get_or_create_persistent_env(env_name, client, group, testdata):
 @pytest.fixture(scope='session')
 def persistent_env1(client1, group1, testdata):
     return get_or_create_persistent_env('persistent_env1', client1, group1, testdata)
+
+
+@pytest.fixture(scope='session')
+def session_env1_aws_client(session_env1, session_id, session_env1_integration_role_arn):
+    try:
+        base_session = boto3.Session()
+        response = base_session.client('sts', region_name=session_env1.region).assume_role(
+            RoleArn=session_env1_integration_role_arn, RoleSessionName=session_env1_integration_role_arn.split('/')[1]
+        )
+        yield boto3.Session(
+            aws_access_key_id=response['Credentials']['AccessKeyId'],
+            aws_secret_access_key=response['Credentials']['SecretAccessKey'],
+            aws_session_token=response['Credentials']['SessionToken'],
+        )
+    except:
+        log.exception('Failed to assume environment integration test role')
+        raise
+
+
+@pytest.fixture(scope='session')
+def persistent_env1_integration_role_arn(persistent_env1):
+    yield f'arn:aws:iam::{persistent_env1.AwsAccountId}:role/dataall-integration-tests-role-{persistent_env1.region}'
+
+
+@pytest.fixture(scope='session')
+def persistent_env1_aws_client(persistent_env1, session_id):
+    try:
+        base_session = boto3.Session()
+        role_arn = (
+            f'arn:aws:iam::{persistent_env1.AwsAccountId}:role/dataall-integration-tests-role-{persistent_env1.region}'
+        )
+        response = base_session.client('sts', region_name=persistent_env1.region).assume_role(
+            RoleArn=role_arn, RoleSessionName=role_arn.split('/')[1]
+        )
+        yield boto3.Session(
+            aws_access_key_id=response['Credentials']['AccessKeyId'],
+            aws_secret_access_key=response['Credentials']['SecretAccessKey'],
+            aws_session_token=response['Credentials']['SessionToken'],
+        )
+    except:
+        log.exception('Failed to assume environment integration test role')
+        raise
