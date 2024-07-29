c22e481c396f8baeca92b7c7509dae34f5861a24
Sofia Sazonova <sazonova@amazon.co.uk>
2024-07-29 16:04:45 +0100

    all enums automatically have queries

--- backend/dataall/modules/maintenance/api/queries.py
+++ backend/dataall/modules/maintenance/api/queries.py
@@ -1,8 +1,6 @@
 """The module defines GraphQL queries for the Maintenance Activity>"""
 
 from dataall.base.api import gql
-from dataall.base.utils.enum_utils import generate_enum_query
-from dataall.modules.maintenance.api.enums import MaintenanceModes, MaintenanceStatus
 from dataall.modules.maintenance.api.resolvers import get_maintenance_window_status
 
 
@@ -10,6 +8,3 @@ getMaintenanceWindowStatus = gql.QueryField(
     name='getMaintenanceWindowStatus', type=gql.Ref('Maintenance'), resolver=get_maintenance_window_status
 )
 
-queryMaintenanceModes = generate_enum_query(MaintenanceModes, 'Maintenance')
-
-queryMaintenanceStatus = generate_enum_query(MaintenanceStatus, 'Maintenance')
