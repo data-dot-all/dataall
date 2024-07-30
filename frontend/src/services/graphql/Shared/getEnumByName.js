ac03c01cb6e079f3408c06004219f165926c3d61
Sofia Sazonova <sazonova@amazon.co.uk>
2024-07-30 11:17:36 +0100

    multiple enums in one request

--- frontend/src/services/graphql/Shared/getEnumByName.js
+++ frontend/src/services/graphql/Shared/getEnumByName.js
@@ -27,3 +27,18 @@ export const fetchOneEnum = async (client, enum_name) => {
     return [];
   }
 };
+
+export const fetchEnums = async (client, enum_names) => {
+  const response = await client.query(
+    getEnumByName({ enum_names: enum_names })
+  );
+  let result = {};
+  if (!response.errors && response.data.queryEnum != null) {
+    for (let i = 0; i < response.data.queryEnum.length; i++) {
+      result[response.data.queryEnum[i].name] =
+        response.data.queryEnum[i].items;
+    }
+  } else {
+    return result;
+  }
+};
