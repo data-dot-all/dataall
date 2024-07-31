22a465c8be4db512ee609e680979cfad10099aa0
Sofia Sazonova <sazonova@amazon.co.uk>
2024-07-31 10:21:16 +0100

    semgrep scan fix

--- frontend/src/services/graphql/Shared/getEnumByName.js
+++ frontend/src/services/graphql/Shared/getEnumByName.js
@@ -26,11 +26,11 @@ export const fetchEnums = async (client, enum_names) => {
   const response = await client.query(
     getEnumByName({ enum_names: enum_names })
   );
+  let enum_dict = {};
   if (!response.errors && response.data.queryEnums != null) {
-    return Object.assign(
-      {},
-      ...response.data.queryEnums.map((x) => ({ [x.name]: x.items }))
-    );
+    response.data.queryEnums.map((x) => {
+      enum_dict[x.name] = x.items;
+    });
   }
-  return {};
+  return enum_dict;
 };
