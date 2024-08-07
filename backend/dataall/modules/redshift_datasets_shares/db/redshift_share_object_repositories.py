3168786a4f9c0e0ca463d7a711dab2f6e79d4698
dlpzx <dlpzx@amazon.com>
2024-08-07 09:17:48 +0200

    PR REVIEW - filter count resources of redshift shares to count only shared items

--- backend/dataall/modules/redshift_datasets_shares/db/redshift_share_object_repositories.py
+++ backend/dataall/modules/redshift_datasets_shares/db/redshift_share_object_repositories.py
@@ -7,6 +7,7 @@ from dataall.modules.shares_base.services.shares_enums import (
     PrincipalType,
 )
 from dataall.modules.shares_base.db.share_object_models import ShareObjectItem, ShareObject
+from dataall.modules.shares_base.db.share_state_machines_repositories import ShareStatusRepository
 
 logger = logging.getLogger(__name__)
 
@@ -15,10 +16,7 @@ class RedshiftShareEnvironmentResource(EnvironmentResource):
     @staticmethod
     def count_resources(session, environment, group_uri) -> int:
         return RedshiftShareObjectRepository.count_redshift_principal_shares(
-            session=session,
-            environment_uri=environment.environmentUri,
-            group_uri=group_uri,
-            principal_type=PrincipalType.RedshiftRole,
+            session=session, environment_uri=environment.environmentUri, group_uri=group_uri
         )
 
     @staticmethod
@@ -28,7 +26,12 @@ class RedshiftShareEnvironmentResource(EnvironmentResource):
 
 class RedshiftShareObjectRepository:
     @staticmethod
-    def count_redshift_principal_shares(session, environment_uri: str, group_uri: str, principal_type: PrincipalType):
+    def count_redshift_principal_shares(session, environment_uri: str, group_uri: str):
+        """
+        Count RedshiftTables currently shared with Redshift Roles for a particular
+        environment on behalf of a particular group
+        """
+        share_item_shared_states = ShareStatusRepository.get_share_item_shared_states()
         return (
             session.query(ShareObject)
             .join(
@@ -39,8 +42,9 @@ class RedshiftShareObjectRepository:
                 and_(
                     ShareObject.environmentUri == environment_uri,
                     ShareObject.groupUri == group_uri,
-                    ShareObject.principalType == principal_type.value,
+                    ShareObject.principalType == PrincipalType.RedshiftRole.value,
                     ShareObjectItem.itemType == ShareableType.RedshiftTable.value,
+                    ShareObjectItem.status.in_(share_item_shared_states),
                 )
             )
             .count()
@@ -48,7 +52,18 @@ class RedshiftShareObjectRepository:
 
     @staticmethod
     def delete_all_redshift_share_items(session, env_uri):
-        env_shared_with_objects = session.query(ShareObject).filter(ShareObject.environmentUri == env_uri).all()
+        """
+        Delete all ShareObjects and ShareObjectItems of Redshift type for a particular environment
+        """
+        env_shared_with_objects = (
+            session.query(ShareObject)
+            .filter(
+                and_(
+                    ShareObject.environmentUri == env_uri, ShareObject.principalType == PrincipalType.RedshiftRole.value
+                )
+            )
+            .all()
+        )
         for share in env_shared_with_objects:
             (
                 session.query(ShareObjectItem)
