e6838adb124477e297e20021e9add9cba73e51d3
Petros Kalos <kalosp@amazon.com>
2024-08-16 16:39:48 +0300

    allow dbmigrations lambda to invoke any alembic command

--- backend/deployment_triggers/dbmigrations_handler.py
+++ backend/deployment_triggers/dbmigrations_handler.py
@@ -15,4 +15,7 @@ logger.setLevel(os.environ.get('LOG_LEVEL', 'INFO'))
 def handler(event, context) -> None:
     alembic_cfg = Config('alembic.ini')
     alembic_cfg.set_main_option('script_location', './migrations')
-    command.upgrade(alembic_cfg, 'head')  # logging breaks after this command
+    event_command = event.get('command', 'upgrade')
+    event_args = event.get('args', {'revision': 'head'})
+    logger.info(f'calling alembic "{event_command}({event_args})"')
+    getattr(command, event_command)(config=alembic_cfg, **event_args)
