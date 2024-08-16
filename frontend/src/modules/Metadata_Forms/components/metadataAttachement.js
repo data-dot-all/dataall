b890942e6bcbfc27e728e733d8b028fbe5d25936
Sofia Sazonova <sazonova@amazon.co.uk>
2024-08-16 10:24:44 +0100

    metadataAttachement form

--- frontend/src/modules/Metadata_Forms/components/metadataAttachement.js
+++ frontend/src/modules/Metadata_Forms/components/metadataAttachement.js
@@ -1,11 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import { useDispatch } from 'react-redux';
 
 import { Box, Card, CardContent, Grid } from '@mui/material';
-import { listMetadataForms } from '../services';
+import { getMetadataForm, listMetadataForms } from '../services';
 import { Defaults } from '../../../design';
 import CircularProgress from '@mui/material/CircularProgress';
+import { useClient } from '../../../services';
+import { RenderedMetadataForm } from './renderedMetadataForm';
+import { SET_ERROR } from '../../../globalErrors';
 
 export const MetadataAttachement = (props) => {
   const client = useClient();
@@ -13,17 +16,34 @@ export const MetadataAttachement = (props) => {
   const [selectedForm, setSelectedForm] = useState(null);
   const [loading, setLoading] = useState(false);
   const [formsList, setFormsList] = useState([]);
+  const [fields, setFields] = useState([]);
   const [filter] = useState(Defaults.filter);
 
   const fetchList = async () => {
     setLoading(true);
     const response = await client.query(listMetadataForms(filter));
     if (!response.errors) {
-      setFormsList(response.data.listMetadataForms);
+      setFormsList(response.data.listMetadataForms.nodes);
     } else {
       dispatch({ type: SET_ERROR, error: response.errors[0].message });
     }
     setLoading(false);
+  };
+
+  const fetchFields = async () => {
+    const response = await client.query(getMetadataForm(selectedForm.uri));
+    if (
+      !response.errors &&
+      response.data &&
+      response.data.getMetadataForm !== null
+    ) {
+      setFields(response.data.getMetadataForm.fields);
+    } else {
+      const error = response.errors
+        ? response.errors[0].message
+        : 'Metadata Forms not found';
+      dispatch({ type: SET_ERROR, error });
+    }
   };
 
   useEffect(() => {
@@ -57,12 +77,18 @@ export const MetadataAttachement = (props) => {
               sx={{
                 backgroundColor: selectedForm === form ? '#e6e6e6' : 'white'
               }}
-              onClick={() => setSelectedForm(form)}
+              onClick={async () => {
+                setSelectedForm(form);
+                await fetchFields();
+              }}
             >
               {form.name}
             </CardContent>
           ))}
         </Card>
+      </Grid>
+      <Grid item lg={9} xl={9} xs={6}>
+        {selectedForm && fields && <RenderedMetadataForm fields={fields} />}
       </Grid>
     </Grid>
   );
