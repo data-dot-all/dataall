ead2a05e6566953fa35c7bf79d10ffb561f4b5fb
Sofia Sazonova <sazonova@amazon.co.uk>
2024-08-27 13:09:07 +0100

    make ruff happy

--- backend/dataall/base/utils/naming_convention.py
+++ backend/dataall/base/utils/naming_convention.py
@@ -14,7 +14,12 @@ class NamingConventionPattern(Enum):
     KMS = {'regex': '[a-zA-Z0-9/_-]+$', 'separator': '-', 'max_length': 63}
     IAM = {'regex': '[^a-zA-Z0-9-_]', 'separator': '-', 'max_length': 63}  # Role names up to 64 chars
     IAM_POLICY = {'regex': '[^a-zA-Z0-9-_]', 'separator': '-', 'max_length': 128}  # Policy names up to 128 chars
-    GLUE = {'regex': '[^a-zA-Z0-9_]', 'separator': '_', 'max_length': 240, 'valid_external_regex': '^[a-zA-Z0-9_]+$'}  # Limit 255 - 15 extra chars buffer
+    GLUE = {
+        'regex': '[^a-zA-Z0-9_]',
+        'separator': '_',
+        'max_length': 240,
+        'valid_external_regex': '^[a-zA-Z0-9_]+$',
+    }  # Limit 255 - 15 extra chars buffer
     GLUE_ETL = {'regex': '[^a-zA-Z0-9-]', 'separator': '-', 'max_length': 52}
     NOTEBOOK = {'regex': '[^a-zA-Z0-9-]', 'separator': '-', 'max_length': 63}
     MLSTUDIO_DOMAIN = {'regex': '[^a-zA-Z0-9-]', 'separator': '-', 'max_length': 63}
