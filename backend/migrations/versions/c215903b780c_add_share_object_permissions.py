fcbf4b77dd4356c2a315e04257d9ee18d9b8301a
Noah Paige <noahpaig@amazon.com>
2024-08-29 15:44:47 -0400

    Write Modify Shares

--- backend/migrations/versions/c215903b780c_add_share_object_permissions.py
+++ backend/migrations/versions/c215903b780c_add_share_object_permissions.py
@@ -1,7 +1,7 @@
 """add_share_object_permissions
 
 Revision ID: c215903b780c
-Revises: 9efe5f7c69a1
+Revises: f87aecc36d39
 Create Date: 2024-08-08 16:19:25.731721
 
 """
@@ -12,7 +12,7 @@ from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision = 'c215903b780c'
-down_revision = '9efe5f7c69a1'
+down_revision = 'f87aecc36d39'
 branch_labels = None
 depends_on = None
 
