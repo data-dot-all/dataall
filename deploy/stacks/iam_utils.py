7af0d788c86f42d2d52c88465cc3b1a2bda2ec58
Noah Paige <noahpaig@amazon.com>
2024-09-05 14:08:04 -0400

    update hash function and rmeove unused impor

--- deploy/stacks/iam_utils.py
+++ deploy/stacks/iam_utils.py
@@ -1,7 +1,6 @@
 from aws_cdk import aws_iam as iam
-import uuid
 import hashlib
 
 
 def get_tooling_account_external_id(account: str):
-    return hashlib.md5(str(account).encode('UTF-8')).hexdigest()[:12]
+    return hashlib.sha256(str(account).encode('UTF-8')).hexdigest()[:12]
