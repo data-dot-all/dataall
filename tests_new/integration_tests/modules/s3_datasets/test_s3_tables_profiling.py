6fe73ef1131f149c71247d58b39044fa97385e94
dlpzx <dlpzx@amazon.com>
2024-09-10 12:17:29 +0200

    Added missing tests for datasets_base and for sr_profiling

--- tests_new/integration_tests/modules/s3_datasets/test_s3_tables_profiling.py
+++ tests_new/integration_tests/modules/s3_datasets/test_s3_tables_profiling.py
@@ -42,21 +42,76 @@ def test_start_table_profiling_unauthorized(client2, dataset_fixture_name, reque
     ).contains('UnauthorizedOperation', 'PROFILE_DATASET_TABLE', dataset_uri)
 
 
-def test_list_table_profiling_runs():
-    # TODO
-    pass
+@pytest.mark.parametrize(
+    'tables_fixture_name',
+    [
+        'session_s3_dataset1_tables',
+        'session_imported_sse_s3_dataset1_tables',
+        'session_imported_kms_s3_dataset1_tables',
+    ],
+)
+@pytest.mark.dependency(depends=['test_start_table_profiling'])
+def test_list_table_profiling_runs(client1, tables_fixture_name, request):
+    tables = request.getfixturevalue(tables_fixture_name)
+    table = tables[0]
+    response = list_table_profiling_runs(client1, tableUri=table.tableUri)
+    assert_that(response.count).is_equal_to(1)
+    assert_that(response.nodes[0].status).is_in(['STARTING', 'RUNNING', 'SUCCEEDED'])
 
 
-def test_list_table_profiling_runs_unauthorized():
-    # TODO
-    pass
+@pytest.mark.parametrize(
+    'tables_fixture_name, confidentiality',
+    [
+        ('session_s3_dataset1_tables', 'Unclassified'),
+        ('session_imported_sse_s3_dataset1_tables', 'Official'),
+        ('session_imported_kms_s3_dataset1_tables', 'Secret'),
+    ],
+)
+@pytest.mark.dependency(depends=['test_start_table_profiling'])
+def test_list_table_profiling_runs_by_confidentiality(client2, tables_fixture_name, confidentiality, request):
+    tables = request.getfixturevalue(tables_fixture_name)
+    table_uri = tables[0].tableUri
+    if confidentiality in ['Unclassified']:
+        response = list_table_profiling_runs(client2, tableUri=table_uri)
+        assert_that(response.count).is_equal_to(1)
+    else:
+        assert_that(list_table_profiling_runs).raises(GqlError).when_called_with(client2, table_uri).contains(
+            'UnauthorizedOperation', 'GET_TABLE_PROFILING_METRICS'
+        )
 
 
-def test_get_table_profiling_run():
-    # TODO
-    pass
+@pytest.mark.parametrize(
+    'tables_fixture_name',
+    [
+        'session_s3_dataset1_tables',
+        'session_imported_sse_s3_dataset1_tables',
+        'session_imported_kms_s3_dataset1_tables',
+    ],
+)
+@pytest.mark.dependency(depends=['test_start_table_profiling'])
+def test_get_table_profiling_run(client1, tables_fixture_name, request):
+    tables = request.getfixturevalue(tables_fixture_name)
+    table = tables[0]
+    response = get_table_profiling_run(client1, tableUri=table.tableUri)
+    assert_that(response.status).is_in(['STARTING', 'RUNNING', 'SUCCEEDED'])
 
 
-def test_get_table_profiling_run_unauthorized():
-    # TODO
-    pass
+@pytest.mark.parametrize(
+    'tables_fixture_name, confidentiality',
+    [
+        ('session_s3_dataset1_tables', 'Unclassified'),
+        ('session_imported_sse_s3_dataset1_tables', 'Official'),
+        ('session_imported_kms_s3_dataset1_tables', 'Secret'),
+    ],
+)
+@pytest.mark.dependency(depends=['test_start_table_profiling'])
+def test_get_table_profiling_run_by_confidentiality(client2, tables_fixture_name, confidentiality, request):
+    tables = request.getfixturevalue(tables_fixture_name)
+    table_uri = tables[0].tableUri
+    if confidentiality in ['Unclassified']:
+        response = get_table_profiling_run(client2, tableUri=table_uri)
+        assert_that(response.count).is_equal_to(1)
+    else:
+        assert_that(get_table_profiling_run).raises(GqlError).when_called_with(client2, table_uri).contains(
+            'UnauthorizedOperation', 'GET_TABLE_PROFILING_METRICS'
+        )
