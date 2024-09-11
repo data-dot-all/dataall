1ec4b916ae1223787840d51fe194b8257805c6e1
Sofia Sazonova <sazonova@amazon.co.uk>
2024-09-11 16:05:18 +0100

    migration to backfill MF resource permissions

---
+++ backend/migrations/versions/427db8f31999_backfill_MF_resource_permissions.py
@@ -0,0 +1,66 @@
+"""backfill_MF_resource_permissions
+
+Revision ID: 427db8f31999
+Revises: f87aecc36d39
+Create Date: 2024-09-11 15:55:51.444403
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy import orm
+
+from dataall.core.environment.db.environment_models import EnvironmentGroup, Environment
+from dataall.core.organizations.db.organization_models import OrganizationGroup, Organization
+from dataall.core.permissions.services.resource_policy_service import ResourcePolicyService
+from dataall.modules.datasets_base.db.dataset_models import DatasetBase
+from dataall.modules.metadata_forms.services.metadata_form_permissions import ATTACH_METADATA_FORM, CREATE_METADATA_FORM
+
+# revision identifiers, used by Alembic.
+revision = '427db8f31999'
+down_revision = 'f87aecc36d39'
+branch_labels = None
+depends_on = None
+
+
+def get_session():
+    bind = op.get_bind()
+    session = orm.Session(bind=bind)
+    return session
+
+
+def upgrade():
+    session = get_session()
+    print('Adding organization resource permissions...')
+    orgGroups = session.query(OrganizationGroup).all()
+    for group in orgGroups:
+        ResourcePolicyService.attach_resource_policy(
+            session=session,
+            group=group.groupUri,
+            resource_uri=group.organizationUri,
+            permissions=[ATTACH_METADATA_FORM, CREATE_METADATA_FORM],
+            resource_type=Organization.__name__,
+        )
+    print('Adding environment resource permissions...')
+    envGroups = session.query(EnvironmentGroup).all()
+    for group in envGroups:
+        ResourcePolicyService.attach_resource_policy(
+            session=session,
+            group=group.groupUri,
+            resource_uri=group.environmentUri,
+            permissions=[ATTACH_METADATA_FORM, CREATE_METADATA_FORM],
+            resource_type=Environment.__name__,
+        )
+    print('Adding dataset resource permissions...')
+    datasets = session.query(DatasetBase).all()
+    for dataset in datasets:
+        ResourcePolicyService.attach_resource_policy(
+            session=session,
+            group=dataset.SamlGroupName,
+            resource_uri=dataset.datasetUri,
+            permissions=[ATTACH_METADATA_FORM],
+            resource_type=DatasetBase.__name__,
+        )
+
+
+def downgrade():
+    print('no downgrade supported')
