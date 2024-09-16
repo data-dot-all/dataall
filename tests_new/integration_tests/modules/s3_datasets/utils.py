ae400b15e9be4437a7bcf2f3524f2dbf4aaa95e3
Sofia Sazonova <sazonova@amazon.co.uk>
2024-09-16 17:40:21 +0100

    remove unused

--- tests_new/integration_tests/modules/s3_datasets/utils.py
+++
@@ -1,8 +0,0 @@
-from tests_new.integration_tests.modules.s3_datasets.aws_clients import S3Client
-from tests_new.integration_tests.utils import poller
-
-
-@poller(check_success=lambda resp: resp is not None, timeout=600)
-def check_access_point_exists(client, account, region, access_point_name):
-    s3_client = S3Client(session=client, region=region)
-    return s3_client.get_access_point(account, access_point_name)
