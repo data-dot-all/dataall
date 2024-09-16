9f62e06304c87cb71190bf3b6d316f1baf4f626c
dlpzx <dlpzx@amazon.com>
2024-09-16 12:10:46 +0200

    Added tests for stack logs and key-value tags

--- tests_new/integration_tests/core/stack/queries.py
+++ tests_new/integration_tests/core/stack/queries.py
@@ -56,3 +56,68 @@ def get_stack(client, env_uri, stack_uri, target_uri, target_type):
     }
     response = client.query(query=query)
     return response.data.getStack
+
+
+def get_stack_logs(client, target_uri, target_type):
+    query = {
+        'operationName': 'getStackLogs',
+        'variables': {
+            'targetUri': target_uri,
+            'targetType': target_type,
+        },
+        'query': """
+                query getStackLogs($targetUri: String!, $targetType: String!) {
+                  getStackLogs(targetUri: $targetUri, targetType: $targetType) {
+                    message
+                    timestamp
+                  }
+                }
+                """,
+    }
+    response = client.query(query=query)
+    return response.data.getStackLogs
+
+
+def list_key_value_tags(client, target_uri, target_type):
+    query = {
+        'operationName': 'listKeyValueTags',
+        'variables': {
+            'targetUri': target_uri,
+            'targetType': target_type,
+        },
+        'query': """
+                query listKeyValueTags($targetUri: String!, $targetType: String!) {
+                  listKeyValueTags(targetUri: $targetUri, targetType: $targetType) {
+                    tagUri
+                    targetUri
+                    targetType
+                    key
+                    value
+                    cascade
+                  }
+                }
+                """,
+    }
+    response = client.query(query=query)
+    return response.data.listKeyValueTags
+
+
+def update_key_value_tags(client, input):
+    query = {
+        'operationName': 'updateKeyValueTags',
+        'variables': {'input': input},
+        'query': """
+                mutation updateKeyValueTags($input: UpdateKeyValueTagsInput!) {
+                  updateKeyValueTags(input: $input) {
+                    tagUri
+                    targetUri
+                    targetType
+                    key
+                    value
+                    cascade
+                  }
+                }
+                """,
+    }
+    response = client.query(query=query)
+    return response.data.updateKeyValueTags
