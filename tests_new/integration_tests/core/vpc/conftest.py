960de955990d5bbfa9f01554bb76208c5316ed9f
dlpzx <dlpzx@amazon.com>
2024-09-17 10:45:16 +0200

    Added VPC network integration tests + fix tags bug in networks

---
+++ tests_new/integration_tests/core/vpc/conftest.py
@@ -0,0 +1,17 @@
+import pytest
+from integration_tests.core.vpc.queries import create_network, delete_network
+
+
+@pytest.fixture(scope='function')
+def network1(client1, group1, session_env1, session_id):
+    network = create_network(
+        client1,
+        name='testVpc1',
+        vpc_id='someId',
+        public_subnets=['testSubnet1'],
+        environment_uri=session_env1.environmentUri,
+        group=group1,
+        tags=[session_id],
+    )
+    yield network
+    delete_network(client1, vpc_uri=network.vpcUri)
