data-dot-all/dataall

commit 6f19119dc61cd361ffc6ca2c4336e642e3e79a93

Utils for enum-queries (#1435)
### Feature or Bugfix
<!-- please choose -->
- Refactoring

### Detail
- new api query `queryEnum` to query multiple enums at once by names
- shared frontend util to query several enums (fetchEnums`) by name.
Output -- dictionary
```
{
 'enumName': [{name: '...', value: '..'}]
}
```
- example for Maintenance mode enum

### Relates
- <URL or Ticket>

### Security
Please answer the questions below briefly where applicable, or write
`N/A`. Based on
[OWASP 10](https://owasp.org/Top10/en/).

- Does this PR introduce or modify any input fields or queries - this
includes
fetching data from storage outside the application (e.g. a database, an
S3 bucket)?
  - Is the input sanitized?
- What precautions are you taking before deserializing the data you
consume?
  - Is injection prevented by parametrizing queries?
  - Have you ensured no `eval` or similar functions are used?
- Does this PR introduce any functionality or component that requires
authorization?
- How have you ensured it respects the existing AuthN/AuthZ mechanisms?
  - Are you logging failed auth attempts?
- Are you using or adding any cryptographic features?
  - Do you use a standard proven implementations?
  - Are the used keys controlled by the customer? Where are they stored?
- Are you introducing any new policies/roles/users?
  - Have you used the least-privilege principle? How?


By submitting this pull request, I confirm that my contribution is made
under the terms of the Apache 2.0 license.

---------

Co-authored-by: Sofia Sazonova <sazonova@amazon.co.uk>

frontend/src/modules/Maintenance/components/MaintenanceViewer.js
@@ -27,15 +27,10 @@ import {
   startMaintenanceWindow,
   startReindexCatalog
 } from '../services';
-import { useClient } from 'services';
+import { useClient, fetchEnums } from 'services';
 import { SET_ERROR, useDispatch } from 'globalErrors';
 import { useSnackbar } from 'notistack';
 import { ModuleNames, isModuleEnabled } from 'utils';
-
-const maintenanceModes = [
-  { value: 'READ-ONLY', label: 'Read-Only' },
-  { value: 'NO-ACCESS', label: 'No-Access' }
-];
 
 const START_MAINTENANCE = 'Start Maintenance';
 const END_MAINTENANCE = 'End Maintenance';
@@ -314,10 +309,24 @@ export const MaintenanceViewer = () => {
     useState(START_MAINTENANCE);
   const [maintenanceWindowStatus, setMaintenanceWindowStatus] =
     useState(INACTIVE_STATUS);
+  const [maintenanceModes, setMaintenanceModes] = useState([]);
   const [dropDownStatus, setDropDownStatus] = useState(false);
   const [refreshingTimer, setRefreshingTimer] = useState('');
   const { enqueueSnackbar, closeSnackbar } = useSnackbar();
   const dispatch = useDispatch();
+
+  const fetchMaintenanceModes = async () => {
+    const maintenanceModesEnum = await fetchEnums(client, ['MaintenanceModes']);
+    if (maintenanceModesEnum['MaintenanceModes'].length > 0) {
+      setMaintenanceModes(
+        maintenanceModesEnum['MaintenanceModes'].map((elem) => {
+          return { label: elem.value, value: elem.name };
+        })
+      );
+    } else {
+      dispatch({ type: SET_ERROR, error: 'Could not fetch maintenance modes' });
+    }
+  };
 
   const refreshMaintenanceView = async () => {
     setUpdating(true);
@@ -467,6 +476,9 @@ export const MaintenanceViewer = () => {
     if (client) {
       initializeMaintenanceView().catch((e) =>
         dispatch({ type: SET_ERROR, e })
+      );
+      fetchMaintenanceModes().catch((e) =>
+        dispatch({ type: SET_ERROR, error: e.message })
       );
       const setTimer = setInterval(() => {
         refreshStatus().catch((e) =>

backend/dataall/base/api/__init__.py
@@ -12,6 +12,7 @@ from ariadne import (
 
 from dataall.base.api import gql
 from dataall.base.api.constants import GraphQLEnumMapper
+from dataall.base.api.queries import enumsQuery
 
 
 def bootstrap():

backend/dataall/base/api/queries.py
@@ -1,0 +1,11 @@
+from dataall.base.api import gql
+from dataall.base.api.resolvers import enum_resolver
+from dataall.base.api.types import EnumResult
+
+enumsQuery = gql.QueryField(
+    name='queryEnums',
+    args=[gql.Argument(name='enums_names', type=gql.ArrayType(gql.String))],
+    type=gql.ArrayType(EnumResult),
+    resolver=enum_resolver,
+    test_scope='Enums',
+)

backend/dataall/base/api/resolvers.py
@@ -1,0 +1,14 @@
+from dataall.base.api.constants import GraphQLEnumMapper
+
+
+def enum_resolver(context, source, enums_names):
+    result = []
+    for enum_class in GraphQLEnumMapper.__subclasses__():
+        if enum_class.__name__ in enums_names:
+            result.append(
+                {
+                    'name': enum_class.__name__,
+                    'items': [{'name': item.name, 'value': str(item.value)} for item in enum_class],
+                }
+            )
+    return result

backend/dataall/base/api/types.py
@@ -1,0 +1,17 @@
+from dataall.base.api import gql
+
+EnumItem = gql.ObjectType(
+    name='EnumItem',
+    fields=[
+        gql.Field(name='name', type=gql.String),
+        gql.Field(name='value', type=gql.String),
+    ],
+)
+
+EnumResult = gql.ObjectType(
+    name='EnumResult',
+    fields=[
+        gql.Field(name='name', type=gql.String),
+        gql.Field(name='items', type=gql.ArrayType(EnumItem)),
+    ],
+)

frontend/src/services/graphql/Shared/getEnumByName.js
@@ -1,0 +1,36 @@
+import { gql } from 'apollo-boost';
+
+const getEnumByName = ({ enum_names }) => ({
+  variables: {
+    enum_names: enum_names
+  },
+  query: gql`
+    query queryEnums($enum_names: [String]) {
+      queryEnums(enums_names: $enum_names) {
+        name
+        items {
+          name
+          value
+        }
+      }
+    }
+  `
+});
+
+/// function to fetch multiple enums
+/// output -- dictionary
+// {
+//  'enumName': [{name: '...', value: '..'}]
+// }
+export const fetchEnums = async (client, enum_names) => {
+  const response = await client.query(
+    getEnumByName({ enum_names: enum_names })
+  );
+  if (!response.errors && response.data.queryEnums != null) {
+    return Object.assign(
+      {},
+      ...response.data.queryEnums.map((x) => ({ [x.name]: x.items }))
+    );
+  }
+  return {};
+};

frontend/src/services/graphql/Shared/index.js
@@ -1,0 +1,1 @@
+export * from './getEnumByName';

frontend/src/services/graphql/index.js
@@ -13,6 +13,7 @@ export * from './Notification';
 export * from './Organization';
 export * from './Principal';
 export * from './SavedQuery';
+export * from './Shared';
 export * from './Search';
 export * from './ShareObject';
 export * from './Stack';

commit 93bd073d0d694de6c64a3fd336f39c88f89a0940

fix setting maintenance modes enum (#1567)
### Feature or Bugfix
<!-- please choose -->
- Bugfix


### Detail
- Fix small bug introduced in
https://github.com/data-dot-all/dataall/pull/1435 when fetching enums
specifically for `maintenanceModes`


### Relates


### Security
Please answer the questions below briefly where applicable, or write
`N/A`. Based on
[OWASP 10](https://owasp.org/Top10/en/).

- Does this PR introduce or modify any input fields or queries - this
includes
fetching data from storage outside the application (e.g. a database, an
S3 bucket)?
  - Is the input sanitized?
- What precautions are you taking before deserializing the data you
consume?
  - Is injection prevented by parametrizing queries?
  - Have you ensured no `eval` or similar functions are used?
- Does this PR introduce any functionality or component that requires
authorization?
- How have you ensured it respects the existing AuthN/AuthZ mechanisms?
  - Are you logging failed auth attempts?
- Are you using or adding any cryptographic features?
  - Do you use a standard proven implementations?
  - Are the used keys controlled by the customer? Where are they stored?
- Are you introducing any new policies/roles/users?
  - Have you used the least-privilege principle? How?


By submitting this pull request, I confirm that my contribution is made
under the terms of the Apache 2.0 license.

frontend/src/modules/Maintenance/components/MaintenanceViewer.js
@@ -320,7 +320,7 @@ export const MaintenanceViewer = () => {
     if (maintenanceModesEnum['MaintenanceModes'].length > 0) {
       setMaintenanceModes(
         maintenanceModesEnum['MaintenanceModes'].map((elem) => {
-          return { label: elem.value, value: elem.name };
+          return { label: elem.value, value: elem.value };
         })
       );
     } else {