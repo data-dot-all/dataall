1b5ba8fc891b23dc0eef9b4369857fd8d9a2abc2
Noah Paige <noahpaig@amazon.com>
2024-09-25 12:49:24 -0700

    Skeletion notifications

---
+++ tests_new/integration_tests/modules/notifications/queries.py
@@ -0,0 +1,100 @@
+# TODO: This file will be replaced by using the SDK directly
+
+
+def mark_notification_read(client, uri):
+    query = {
+        'operationName': 'markNotificationAsRead',
+        'variables': {'notificationUri': uri},
+        'query': """
+            mutation markNotificationAsRead($notificationUri: String!) {
+              markNotificationAsRead(notificationUri: $notificationUri)
+            }
+                """,
+    }
+    response = client.query(query=query)
+    return response.data.markNotificationAsRead
+
+
+def delete_notification(client, uri):
+    query = {
+        'operationName': 'deleteNotification',
+        'variables': {'notificationUri': uri},
+        'query': """
+            mutation deleteNotification($notificationUri: String!) {
+              deleteNotification(notificationUri: $notificationUri)
+            }
+                """,
+    }
+    response = client.query(query=query)
+    return response.data.deleteNotification
+
+
+## TODO
+def list_notifications(client):
+    query = {
+        'operationName': 'listNotifications',
+        'variables': {'filter': {'term': ''}},
+        'query': """
+            query listNotifications($filter: NotificationFilter) {
+              listNotifications(filter: $filter) {
+                count
+                page
+                pages
+                hasNext
+                hasPrevious
+                nodes {
+                  notificationUri
+                  message
+                  type
+                  is_read
+                  target_uri
+                }
+              }
+            }
+                """,
+    }
+    response = client.query(query=query)
+    return response.data.listNotifications
+
+
+def count_unread_notificiations(client):
+    query = {
+        'operationName': 'countUnreadNotifications',
+        'variables': {},
+        'query': """
+            query countUnreadNotifications {
+              countUnreadNotifications
+            }
+                """,
+    }
+    response = client.query(query=query)
+    return response.data.countUnreadNotifications
+
+
+## TODO
+def count_read_notificiations(client):
+    query = {
+        'operationName': 'countReadNotifications',
+        'variables': {},
+        'query': """
+            query countReadNotifications {
+              countReadNotifications
+            }
+                """,
+    }
+    response = client.query(query=query)
+    return response.data.countReadNotifications
+
+
+def count_deleted_notificiations(client):
+    query = {
+        'operationName': 'countDeletedNotifications',
+        'variables': {},
+        'query': """
+            query countDeletedNotifications {
+              countDeletedNotifications
+            }
+                """,
+    }
+    response = client.query(query=query)
+    return response.data.countUnreadNotifications
