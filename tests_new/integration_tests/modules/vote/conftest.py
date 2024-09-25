765d497b39aa7c86297840d896dd7f9ba2fbbaa1
Noah Paige <noahpaig@amazon.com>
2024-09-25 13:00:57 -0700

    Switc persistent dataset to session dataset

--- tests_new/integration_tests/modules/vote/conftest.py
+++ tests_new/integration_tests/modules/vote/conftest.py
@@ -6,6 +6,6 @@ S3_DATASET_TARGET_TYPE = 'dataset'
 
 
 @pytest.fixture(scope='session')
-def vote1(client1, persistent_s3_dataset1):
-    upvote(client1, persistent_s3_dataset1.datasetUri, S3_DATASET_TARGET_TYPE, True)
-    yield get_vote(client1, persistent_s3_dataset1.datasetUri, S3_DATASET_TARGET_TYPE)
+def vote1(client1, session_s3_dataset1):
+    upvote(client1, session_s3_dataset1.datasetUri, S3_DATASET_TARGET_TYPE, True)
+    yield get_vote(client1, session_s3_dataset1.datasetUri, S3_DATASET_TARGET_TYPE)
--- tests_new/integration_tests/modules/vote/test_vote.py
+++ tests_new/integration_tests/modules/vote/test_vote.py
@@ -10,15 +10,15 @@ def test_upvote(client1, vote1):
     assert_that(vote1.upvote).is_true()
 
 
-def test_count_votes(client2, vote1, persistent_s3_dataset1):
-    count = count_upvotes(client2, persistent_s3_dataset1.datasetUri, S3_DATASET_TARGET_TYPE)
+def test_count_votes(client2, vote1, session_s3_dataset1):
+    count = count_upvotes(client2, session_s3_dataset1.datasetUri, S3_DATASET_TARGET_TYPE)
 
     # Assert incremeent by 1
-    upvote(client2, persistent_s3_dataset1.datasetUri, S3_DATASET_TARGET_TYPE, True)
-    assert_that(count_upvotes(client2, persistent_s3_dataset1.datasetUri, S3_DATASET_TARGET_TYPE)).is_equal_to(
+    upvote(client2, session_s3_dataset1.datasetUri, S3_DATASET_TARGET_TYPE, True)
+    assert_that(count_upvotes(client2, session_s3_dataset1.datasetUri, S3_DATASET_TARGET_TYPE)).is_equal_to(
         count + 1
     )
 
     # Assert decrement by 1
-    upvote(client2, persistent_s3_dataset1.datasetUri, S3_DATASET_TARGET_TYPE, False)
-    assert_that(count_upvotes(client2, persistent_s3_dataset1.datasetUri, S3_DATASET_TARGET_TYPE)).is_equal_to(count)
+    upvote(client2, session_s3_dataset1.datasetUri, S3_DATASET_TARGET_TYPE, False)
+    assert_that(count_upvotes(client2, session_s3_dataset1.datasetUri, S3_DATASET_TARGET_TYPE)).is_equal_to(count)
