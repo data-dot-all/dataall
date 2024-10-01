09319498bde11c3d6f713eddc5c8451e11ccae06
Noah Paige <69586985+noah-paige@users.noreply.github.com>
2024-10-01 17:02:16 -0400

    add integration tests votes (#1578)
    ### Feature or Bugfix
    <!-- please choose -->
    - Feature
    
    ### Detail
    - Add integration tests for `vote` module
    - Fix Vote creation by username
    
    
    ### Relates
    - https://github.com/data-dot-all/dataall/issues/1220
    
    ### Security
    Please answer the questions below briefly where applicable, or write
    `N/A`. Based on
    [OWASP 10](https://owasp.org/Top10/en/).
    
    - Does this PR introduce or modify any input fields or queries - this
    includes
    fetching data from storage outside the application (e.g. a database, an
    S3 bucket)?
      - Is the input sanitized?
    - What precautions are you taking before deserializing the data you
    consume?
      - Is injection prevented by parametrizing queries?
      - Have you ensured no `eval` or similar functions are used?
    - Does this PR introduce any functionality or component that requires
    authorization?
    - How have you ensured it respects the existing AuthN/AuthZ mechanisms?
      - Are you logging failed auth attempts?
    - Are you using or adding any cryptographic features?
      - Do you use a standard proven implementations?
      - Are the used keys controlled by the customer? Where are they stored?
    - Are you introducing any new policies/roles/users?
      - Have you used the least-privilege principle? How?
    
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache 2.0 license.

---
+++ tests_new/integration_tests/modules/vote/test_vote.py
@@ -0,0 +1,51 @@
+from assertpy import assert_that
+
+from integration_tests.errors import GqlError
+
+from integration_tests.modules.vote.queries import upvote, count_upvotes, get_vote
+from integration_tests.modules.vote.conftest import S3_DATASET_TARGET_TYPE
+
+
+def test_upvote(client1, vote1):
+    assert_that(vote1).is_not_none()
+    assert_that(vote1.voteUri).is_not_none()
+    assert_that(vote1.upvote).is_true()
+
+
+def test_upvote_invalid(client1, vote1, session_s3_dataset1):
+    assert_that(upvote).raises(GqlError).when_called_with(client1, session_s3_dataset1.datasetUri, None, True).contains(
+        'targetType', 'not to be None'
+    )
+    assert_that(upvote).raises(GqlError).when_called_with(client1, None, S3_DATASET_TARGET_TYPE, True).contains(
+        'targetUri', 'not to be None'
+    )
+
+
+def test_get_vote_invalid(client1, vote1, session_s3_dataset1):
+    assert_that(get_vote).raises(GqlError).when_called_with(client1, session_s3_dataset1.datasetUri, None).contains(
+        'targetType', 'must not be null'
+    )
+    assert_that(get_vote).raises(GqlError).when_called_with(client1, None, S3_DATASET_TARGET_TYPE).contains(
+        'targetUri', 'must not be null'
+    )
+
+
+def test_count_upvote_invalid(client1, vote1, session_s3_dataset1):
+    assert_that(count_upvotes).raises(GqlError).when_called_with(
+        client1, session_s3_dataset1.datasetUri, None
+    ).contains('targetType', 'must not be null')
+    assert_that(count_upvotes).raises(GqlError).when_called_with(client1, None, S3_DATASET_TARGET_TYPE).contains(
+        'targetUri', 'must not be null'
+    )
+
+
+def test_count_votes(client2, vote1, session_s3_dataset1):
+    count = count_upvotes(client2, session_s3_dataset1.datasetUri, S3_DATASET_TARGET_TYPE)
+
+    # Assert incremeent by 1
+    upvote(client2, session_s3_dataset1.datasetUri, S3_DATASET_TARGET_TYPE, True)
+    assert_that(count_upvotes(client2, session_s3_dataset1.datasetUri, S3_DATASET_TARGET_TYPE)).is_equal_to(count + 1)
+
+    # Assert decrement by 1
+    upvote(client2, session_s3_dataset1.datasetUri, S3_DATASET_TARGET_TYPE, False)
+    assert_that(count_upvotes(client2, session_s3_dataset1.datasetUri, S3_DATASET_TARGET_TYPE)).is_equal_to(count)
