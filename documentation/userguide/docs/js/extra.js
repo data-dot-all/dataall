fd70d4d86ab6440d8867b0a9903325d18faaf079
Noah Paige <noahpaig@amazon.com>
2024-10-02 17:40:51 -0400

    Fix signout button

--- documentation/userguide/docs/js/extra.js
+++ documentation/userguide/docs/js/extra.js
@@ -4,7 +4,17 @@ document.addEventListener('DOMContentLoaded', function() {
       var button = document.createElement('a');
       button.textContent = 'Sign Out';
       button.className = 'md-header-nav__button md-icon signout-button';
-      button.href = '/signout';
+
+      // Button Click Event Listener
+      button.addEventListener('click', function(event) {
+        event.preventDefault();
+        try {
+          // Handle sign-out logic here
+          window.location.href = '/signout';
+        } catch (error) {
+          console.error('Error during sign-out:', error);
+        }
+      });
       header.appendChild(button);
   }
 });
