eaa2d534d78eebd54430226392b5d8a320d8f074
dlpzx <dlpzx@amazon.com>
2024-10-11 13:25:08 +0200

    Fix assertion in test_redshift_schema_tables + clean PR

--- tests_new/integration_tests/modules/redshift_datasets/global_conftest.py
+++ tests_new/integration_tests/modules/redshift_datasets/global_conftest.py
@@ -1,7 +1,7 @@
 import logging
 
 import pytest
-from integration_tests.core.stack.utils import check_stack_ready, wait_stack_delete_complete
+from integration_tests.core.stack.utils import check_stack_ready
 from integration_tests.conftest import RedshiftConnection
 from integration_tests.modules.redshift_datasets.connection_queries import (
     create_redshift_connection,
