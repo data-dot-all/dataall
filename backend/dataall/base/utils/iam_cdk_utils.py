fe6c1fea9c9e700762c9342a72ac43f56e453396
Tejas Rajopadhye <rajopadhye.tejas@gmail.com>
2024-10-21 19:28:47 -0500

    Adding converter file

---
+++ backend/dataall/base/utils/iam_cdk_utils.py
@@ -0,0 +1,21 @@
+from typing import Dict, Any
+from aws_cdk import aws_iam as iam
+
+
+def convert_from_json_to_iam_policy_statement_with_conditions(iam_policy: Dict[Any, Any]):
+    return iam.PolicyStatement(
+        sid=iam_policy.get('Sid'),
+        effect=iam_policy.get('Effect'),
+        actions=iam_policy.get('Action'),
+        resources=iam_policy.get('Resource'),
+        conditions=iam_policy.get('Condition')
+    )
+
+
+def convert_from_json_to_iam_policy_statement(iam_policy: Dict[Any, Any]):
+    return iam.PolicyStatement(
+        sid=iam_policy.get('Sid'),
+        effect=iam_policy.get('Effect'),
+        actions=iam_policy.get('Action'),
+        resources=iam_policy.get('Resource'),
+    )
