acc6ab5d57d620688e19eaa38bb85fdb50aac1a5
Sofia Sazonova <sazonova@amazon.co.uk>
2024-10-22 16:07:27 +0100

    entityType : Dataset => S3-Dataset

--- frontend/src/design/components/popovers/NotificationsPopover.js
+++ frontend/src/design/components/popovers/NotificationsPopover.js
@@ -103,7 +103,7 @@ export const NotificationsPopover = () => {
 
   const resolve_link = (notification) => {
     if (notification.type === 'METADATA_FORM_UPDATE') {
-      const entity_type = notification.target_uri.split('|')[1].toLowerCase();
+      let entity_type = notification.target_uri.split('|')[1].toLowerCase();
       const entity_uri = notification.target_uri.split('|')[0];
       const main_modules = [
         'environment',
@@ -115,6 +115,7 @@ export const NotificationsPopover = () => {
         'pipeline',
         'notebook'
       ];
+
       if (main_modules.includes(entity_type)) {
         return `/console/${entity_type}s/${entity_uri}`;
       }
