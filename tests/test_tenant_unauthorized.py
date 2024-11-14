75d30c148250894bf791db2bef905c347ed8a3b0
dlpzx <dlpzx@amazon.com>
2024-11-14 15:24:19 +0100

    Invert logic and add assertion to check resolver is not None

--- tests/test_tenant_unauthorized.py
+++ tests/test_tenant_unauthorized.py
@@ -4,35 +4,150 @@ from assertpy import assert_that
 from dataall.base.api import bootstrap
 from dataall.base.loader import load_modules, ImportMode
 from dataall.base.context import RequestContext
-from threading import local
 from dataall.base.db.exceptions import TenantUnauthorized
 import inspect
 
-from pytest import fixture
 
 load_modules(modes={ImportMode.API})
 
-CHECK_PERMS = ['archiveOrganization']
+NO_CHECK_PERMS = [
+    'Mutation.updateGroupTenantPermissions',
+    'Mutation.updateSSMParameter',
+    'Mutation.createNetwork',
+    'Mutation.deleteNetwork',
+    'Mutation.updateStack',
+    'Mutation.updateKeyValueTags',
+    'Mutation.createMetadataForm',
+    'Mutation.createMetadataFormVersion',
+    'Mutation.createAttachedMetadataForm',
+    'Mutation.deleteMetadataForm',
+    'Mutation.deleteMetadataFormVersion',
+    'Mutation.deleteAttachedMetadataForm',
+    'Mutation.createMetadataFormFields',
+    'Mutation.deleteMetadataFormField',
+    'Mutation.batchMetadataFormFieldUpdates',
+    'Mutation.startMaintenanceWindow',
+    'Mutation.stopMaintenanceWindow',
+    'Mutation.markNotificationAsRead',
+    'Mutation.deleteNotification',
+    'Mutation.postFeedMessage',
+    'Mutation.createGlossary',
+    'Mutation.updateGlossary',
+    'Mutation.deleteGlossary',
+    'Mutation.createCategory',
+    'Mutation.updateCategory',
+    'Mutation.deleteCategory',
+    'Mutation.createTerm',
+    'Mutation.updateTerm',
+    'Mutation.deleteTerm',
+    'Mutation.approveTermAssociation',
+    'Mutation.dismissTermAssociation',
+    'Mutation.startReindexCatalog',
+    'Mutation.createShareObject',
+    'Mutation.deleteShareObject',
+    'Mutation.cancelShareExtension',
+    'Mutation.addSharedItem',
+    'Mutation.removeSharedItem',
+    'Mutation.submitShareObject',
+    'Mutation.submitShareExtension',
+    'Mutation.approveShareObject',
+    'Mutation.approveShareExtension',
+    'Mutation.rejectShareObject',
+    'Mutation.revokeItemsShareObject',
+    'Mutation.verifyItemsShareObject',
+    'Mutation.reApplyItemsShareObject',
+    'Mutation.updateShareRejectReason',
+    'Mutation.updateShareExpirationPeriod',
+    'Mutation.updateShareExtensionReason',
+    'Mutation.updateShareRequestReason',
+    'Mutation.updateShareItemFilters',
+    'Mutation.removeShareItemFilter',
+    'Mutation.upVote',
+]
 
+CHECK_PERMS = [
+    'Mutation.createOrganization',
+    'Mutation.updateOrganization',
+    'Mutation.archiveOrganization',
+    'Mutation.inviteGroupToOrganization',
+    'Mutation.updateOrganizationGroup',
+    'Mutation.removeGroupFromOrganization',
+    'Mutation.createEnvironment',
+    'Mutation.updateEnvironment',
+    'Mutation.inviteGroupOnEnvironment',
+    'Mutation.addConsumptionRoleToEnvironment',
+    'Mutation.updateGroupEnvironmentPermissions',
+    'Mutation.removeGroupFromEnvironment',
+    'Mutation.removeConsumptionRoleFromEnvironment',
+    'Mutation.deleteEnvironment',
+    'Mutation.enableDataSubscriptions',
+    'Mutation.DisableDataSubscriptions',
+    'Mutation.updateConsumptionRole',
+    'Mutation.createSagemakerStudioUser',
+    'Mutation.deleteSagemakerStudioUser',
+    'Mutation.createSagemakerNotebook',
+    'Mutation.startSagemakerNotebook',
+    'Mutation.stopSagemakerNotebook',
+    'Mutation.deleteSagemakerNotebook',
+    # 'Mutation.syncDatasetTableColumns',
+    # 'Mutation.updateDatasetTableColumn',
+    # 'Mutation.startDatasetProfilingRun',
+    # 'Mutation.createDatasetStorageLocation',
+    # 'Mutation.updateDatasetStorageLocation',
+    # 'Mutation.deleteDatasetStorageLocation',
+    # 'Mutation.createDataset',
+    # 'Mutation.updateDataset',
+    # 'Mutation.generateDatasetAccessToken',
+    # 'Mutation.deleteDataset',
+    # 'Mutation.importDataset',
+    # 'Mutation.startGlueCrawler',
+    # 'Mutation.updateDatasetTable',
+    # 'Mutation.deleteDatasetTable',
+    # 'Mutation.syncTables',
+    # 'Mutation.createTableDataFilter',
+    # 'Mutation.deleteTableDataFilter',
+    # 'Mutation.createRedshiftConnection',
+    # 'Mutation.deleteRedshiftConnection',
+    # 'Mutation.addConnectionGroupPermission',
+    # 'Mutation.deleteConnectionGroupPermission',
+    # 'Mutation.importRedshiftDataset',
+    # 'Mutation.updateRedshiftDataset',
+    # 'Mutation.deleteRedshiftDataset',
+    # 'Mutation.addRedshiftDatasetTables',
+    # 'Mutation.deleteRedshiftDatasetTable',
+    # 'Mutation.updateRedshiftDatasetTable',
+    # 'Mutation.importDashboard',
+    # 'Mutation.updateDashboard',
+    # 'Mutation.deleteDashboard',
+    # 'Mutation.requestDashboardShare',
+    # 'Mutation.approveDashboardShare',
+    # 'Mutation.rejectDashboardShare',
+    # 'Mutation.createQuicksightDataSourceSet',
+    # 'Mutation.verifyDatasetShareObjects',
+    # 'Mutation.reApplyShareObjectItemsOnDataset',
+    # 'Mutation.createWorksheet',
+    # 'Mutation.updateWorksheet',
+    # 'Mutation.deleteWorksheet'
+]
 
-@fixture(scope='function')
-def patch_context(mocker, db, userNoTenantPermissions, groupNoTenantPermissions):
-    mock = local()
-    mock.context = RequestContext(
+ALL_RESOLVERS = {
+    f'{_type.name}.{field.name}': field.resolver
+    for _type in bootstrap().types
+    for field in _type.fields
+    if field.resolver
+}
+
+@pytest.mark.parametrize('name,field_resolver', [(name, ALL_RESOLVERS.get(name, None)) for name in CHECK_PERMS])
+def test_unauthorized_tenant_permissions(
+    name, field_resolver, mocker, db, userNoTenantPermissions, groupNoTenantPermissions
+):
+    assert_that(field_resolver).is_not_none()
+    mock_local = MagicMock()
+    mock_local.context = RequestContext(
         db, userNoTenantPermissions.username, [groupNoTenantPermissions.groupUri], userNoTenantPermissions
     )
-    mocker.patch('dataall.base.context._request_storage', mock)
-
-
-@pytest.mark.parametrize(
-    'name,field_resolver',
-    [
-        (f'{_type.name}.{field.name}', field.resolver)
-        for _type in bootstrap().types
-        for field in _type.fields
-        if field.resolver and _type.name in ['Mutation', 'Query'] and field.name in CHECK_PERMS
-    ],
-)
-def test_unauthorized_tenant_permissions(name, field_resolver, patch_context):
-    iargs = {arg: MagicMock() for arg in inspect.signature(field_resolver).parameters.keys()}
-    assert_that(field_resolver).raises(TenantUnauthorized).when_called_with(**iargs).contains('UnauthorizedOperation')
+    with mocker.patch('dataall.base.context._request_storage', mock_local):
+        iargs = {arg: MagicMock() for arg in inspect.signature(field_resolver).parameters.keys()}
+        assert_that(field_resolver).raises(TenantUnauthorized).when_called_with(**iargs).contains(
+            'UnauthorizedOperation'
+        )
