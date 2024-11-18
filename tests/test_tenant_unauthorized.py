2da81d0cd3f637991b7f7714d39e2a58882ce039
dlpzx <dlpzx@amazon.com>
2024-11-18 18:18:24 +0100

    Logic for opt-out mutation tests

--- tests/test_tenant_unauthorized.py
+++ tests/test_tenant_unauthorized.py
@@ -12,142 +12,76 @@ load_modules(modes={ImportMode.API})
 
 ## Those Mutations that are commented out either need to be assessed or fixed. They might not need the check of permissions
 ## or they might require additional work to add permissions or to adjust the tests.
+OPT_OUT_MUTATIONS = [
+    'Mutation.updateGroupTenantPermissions',  #  admin action. No need for tenant permission check
+    'Mutation.updateSSMParameter',  #  admin action. No need for tenant permission check
+    'Mutation.createQuicksightDataSourceSet',  #  admin action. No need for tenant permission check
+    'Mutation.startMaintenanceWindow',  #  admin action. No need for tenant permission check
+    'Mutation.stopMaintenanceWindow',  #  admin action. No need for tenant permission check
+    'Mutation.startReindexCatalog',  #  admin action. No need for tenant permission check
+    'Mutation.markNotificationAsRead',  #  tenant permissions do not apply to user personal notifications.
+    'Mutation.deleteNotification',  #  tenant permissions do not apply to user personal notifications.
+    'Mutation.postFeedMessage',  #  tenant permissions do not apply to user personal feed comments.
+    'Mutation.upVote',  #  tenant permissions do not apply to user personal up votes.
+    'Mutation.createShareObject',  #  follow-up PR. ADD MANAGE_SHARE permission
+    'Mutation.deleteShareObject',
+    'Mutation.cancelShareExtension',
+    'Mutation.addSharedItem',
+    'Mutation.removeSharedItem',
+    'Mutation.submitShareObject',
+    'Mutation.submitShareExtension',
+    'Mutation.approveShareObject',
+    'Mutation.approveShareExtension',
+    'Mutation.rejectShareObject',
+    'Mutation.revokeItemsShareObject',
+    'Mutation.verifyItemsShareObject',
+    'Mutation.reApplyItemsShareObject',
+    'Mutation.updateShareRejectReason',
+    'Mutation.updateShareExpirationPeriod',
+    'Mutation.updateShareExtensionReason',
+    'Mutation.updateShareRequestReason',
+    'Mutation.updateShareItemFilters',
+    'Mutation.removeShareItemFilter',
+    'Mutation.createAttachedMetadataForm',  #  outside of this PR to be able to backport to v2.6.2
+    'Mutation.deleteAttachedMetadataForm',  #  outside of this PR to be able to backport to v2.6.2
+    'Mutation.createRedshiftConnection',  #  outside of this PR to be able to backport to v2.6.2
+    'Mutation.deleteRedshiftConnection',  #  outside of this PR to be able to backport to v2.6.2
+    'Mutation.addConnectionGroupPermission',  #  outside of this PR to be able to backport to v2.6.2
+    'Mutation.deleteConnectionGroupPermission',  #  outside of this PR to be able to backport to v2.6.2
+]
 
-CHECK_PERMS = [
-    # 'Mutation.updateGroupTenantPermissions', ---> admin action. No need for tenant permission check
-    # 'Mutation.updateSSMParameter', ---> admin action. No need for tenant permission check
-    'Mutation.createOrganization',
-    'Mutation.updateOrganization',
-    'Mutation.archiveOrganization',
-    'Mutation.inviteGroupToOrganization',
-    'Mutation.updateOrganizationGroup',
-    'Mutation.removeGroupFromOrganization',
-    'Mutation.createNetwork',
-    'Mutation.deleteNetwork',
-    'Mutation.createEnvironment',
-    'Mutation.updateEnvironment',
-    'Mutation.inviteGroupOnEnvironment',
-    'Mutation.addConsumptionRoleToEnvironment',
-    'Mutation.updateGroupEnvironmentPermissions',
-    'Mutation.removeGroupFromEnvironment',
-    'Mutation.removeConsumptionRoleFromEnvironment',
-    'Mutation.deleteEnvironment',
-    'Mutation.enableDataSubscriptions',
-    'Mutation.DisableDataSubscriptions',
-    'Mutation.updateConsumptionRole',
+OPT_IN_QUERIES = [
     'Query.generateEnvironmentAccessToken',
     'Query.getEnvironmentAssumeRoleUrl',
-    'Mutation.updateStack',
-    'Mutation.updateKeyValueTags',
-    'Mutation.createSagemakerStudioUser',
-    'Mutation.deleteSagemakerStudioUser',
     'Query.getSagemakerStudioUserPresignedUrl',
-    'Mutation.createSagemakerNotebook',
-    'Mutation.startSagemakerNotebook',
-    'Mutation.stopSagemakerNotebook',
-    'Mutation.deleteSagemakerNotebook',
     'Query.getSagemakerNotebookPresignedUrl',
-    'Mutation.createMetadataForm',
-    'Mutation.createMetadataFormVersion',
-    # 'Mutation.createAttachedMetadataForm', ---> outside of this PR to be able to backport to v2.6.2
-    'Mutation.deleteMetadataForm',
-    'Mutation.deleteMetadataFormVersion',
-    # 'Mutation.deleteAttachedMetadataForm', ---> outside of this PR to be able to backport to v2.6.2
-    'Mutation.createMetadataFormFields',
-    'Mutation.deleteMetadataFormField',
-    'Mutation.batchMetadataFormFieldUpdates',
-    # 'Mutation.startMaintenanceWindow',  ---> admin action. No need for tenant permission check
-    # 'Mutation.stopMaintenanceWindow',  ---> admin action. No need for tenant permission check
-    # 'Mutation.markNotificationAsRead', ---> tenant permissions do not apply to user personal notifications.
-    # 'Mutation.deleteNotification', ---> tenant permissions do not apply to user personal notifications.
-    'Mutation.createGlossary',
-    'Mutation.updateGlossary',
-    'Mutation.deleteGlossary',
-    'Mutation.createCategory',
-    'Mutation.updateCategory',
-    'Mutation.deleteCategory',
-    'Mutation.createTerm',
-    'Mutation.updateTerm',
-    'Mutation.deleteTerm',
-    'Mutation.approveTermAssociation',
-    'Mutation.dismissTermAssociation',
-    # 'Mutation.startReindexCatalog',  ---> admin action. No need for tenant permission check
-    # 'Mutation.postFeedMessage', ---> tenant permissions do not apply to user personal feed comments.
-    # 'Mutation.createShareObject', ---> follow-up PR. ADD MANAGE_SHARE permission
-    # 'Mutation.deleteShareObject',
-    # 'Mutation.cancelShareExtension',
-    # 'Mutation.addSharedItem',
-    # 'Mutation.removeSharedItem',
-    # 'Mutation.submitShareObject',
-    # 'Mutation.submitShareExtension',
-    # 'Mutation.approveShareObject',
-    # 'Mutation.approveShareExtension',
-    # 'Mutation.rejectShareObject',
-    # 'Mutation.revokeItemsShareObject',
-    # 'Mutation.verifyItemsShareObject',
-    # 'Mutation.reApplyItemsShareObject',
-    # 'Mutation.updateShareRejectReason',
-    # 'Mutation.updateShareExpirationPeriod',
-    # 'Mutation.updateShareExtensionReason',
-    # 'Mutation.updateShareRequestReason',
-    # 'Mutation.updateShareItemFilters',
-    # 'Mutation.removeShareItemFilter',
-    # 'Mutation.upVote', ---> tenant permissions do not apply to user personal up votes.
-    'Mutation.syncDatasetTableColumns',
-    'Mutation.updateDatasetTableColumn',
-    'Mutation.startDatasetProfilingRun',
-    'Mutation.createDatasetStorageLocation',
-    'Mutation.updateDatasetStorageLocation',
-    'Mutation.deleteDatasetStorageLocation',
-    'Mutation.createDataset',
-    'Mutation.updateDataset',
-    'Mutation.generateDatasetAccessToken',
-    'Mutation.deleteDataset',
-    'Mutation.importDataset',
-    'Mutation.startGlueCrawler',
-    'Mutation.updateDatasetTable',
-    'Mutation.deleteDatasetTable',
-    'Mutation.syncTables',
-    'Mutation.createTableDataFilter',
-    'Mutation.deleteTableDataFilter',
     'Query.getDatasetAssumeRoleUrl',
     'Query.getDatasetPresignedUrl',
-    # 'Mutation.createRedshiftConnection', ---> outside of this PR to be able to backport to v2.6.2
-    # 'Mutation.deleteRedshiftConnection', ---> outside of this PR to be able to backport to v2.6.2
-    # 'Mutation.addConnectionGroupPermission', ---> outside of this PR to be able to backport to v2.6.2
-    # 'Mutation.deleteConnectionGroupPermission', ---> outside of this PR to be able to backport to v2.6.2
-    'Mutation.importRedshiftDataset',
-    'Mutation.updateRedshiftDataset',
-    'Mutation.deleteRedshiftDataset',
-    'Mutation.addRedshiftDatasetTables',
-    'Mutation.deleteRedshiftDatasetTable',
-    'Mutation.updateRedshiftDatasetTable',
-    'Mutation.importDashboard',
-    'Mutation.updateDashboard',
-    'Mutation.deleteDashboard',
-    'Mutation.requestDashboardShare',
-    'Mutation.approveDashboardShare',
-    'Mutation.rejectDashboardShare',
-    # 'Mutation.createQuicksightDataSourceSet',  ---> admin action. No need for tenant permission check
     'Query.getAuthorSession',
-    'Mutation.verifyDatasetShareObjects',
-    'Mutation.reApplyShareObjectItemsOnDataset',
     'Query.getDatasetSharedAssumeRoleUrl',
-    'Mutation.createWorksheet',
-    'Mutation.updateWorksheet',
-    'Mutation.deleteWorksheet',
     'Query.runAthenaSqlQuery',
 ]
 
-ALL_RESOLVERS = {
+ALL_QUERIES = {
     f'{_type.name}.{field.name}': field.resolver
     for _type in bootstrap().types
     for field in _type.fields
-    if field.resolver
+    if field.resolver and _type.name == 'Query'
+}
+
+ALL_MUTATIONS = {
+    f'{_type.name}.{field.name}': field.resolver
+    for _type in bootstrap().types
+    for field in _type.fields
+    if field.resolver and _type.name == 'Mutation'
 }
 
 
-@pytest.mark.parametrize('name,field_resolver', [(name, ALL_RESOLVERS.get(name, None)) for name in CHECK_PERMS])
+@pytest.mark.parametrize(
+    'name,field_resolver',
+    [(key, value) for key, value in ALL_MUTATIONS.items() if key not in OPT_OUT_MUTATIONS]
+    + [(name, ALL_QUERIES.get(name, None)) for name in OPT_IN_QUERIES],
+)
 def test_unauthorized_tenant_permissions(
     name, field_resolver, mocker, db, userNoTenantPermissions, groupNoTenantPermissions
 ):
