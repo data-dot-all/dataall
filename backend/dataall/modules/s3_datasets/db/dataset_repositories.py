bb7d40f4934457dcdef0fdcb46b49a91c9d64ce5
Tejas Rajopadhye <rajopadhye.tejas@gmail.com>
2024-11-19 15:30:00 -0600

    Removing unused comments

--- backend/dataall/modules/s3_datasets/db/dataset_repositories.py
+++ backend/dataall/modules/s3_datasets/db/dataset_repositories.py
@@ -202,8 +202,6 @@ class DatasetRepository(EnvironmentResource):
     def list_all_active_datasets(session) -> [S3Dataset]:
         return session.query(S3Dataset).filter(S3Dataset.deleted.is_(None)).all()
 
-    # Here all dataset with the same db name are fetched irrespective of their environment
-    # Irresepective of environments because the same db name can be used in any requestors account
     @staticmethod
     def list_all_active_datasets_with_glue_db(session, dataset_uri) -> [S3Dataset]:
         s3_dataset: S3Dataset = DatasetRepository.get_dataset_by_uri(session=session, dataset_uri=dataset_uri)
