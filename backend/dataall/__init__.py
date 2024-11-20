d97c7d0c22b6ba12c7fffac7d09977cc034b3641
Noah Paige <noahpaig@amazon.com>
2024-11-20 14:20:58 -0500

    move logger configs for all tasks to single place

--- backend/dataall/__init__.py
+++ backend/dataall/__init__.py
@@ -1,2 +1,13 @@
 from . import core, version
 from .base import utils, db, api
+import logging
+import os
+import sys
+
+logging.basicConfig(
+    level=os.environ.get('LOG_LEVEL', 'INFO'),
+    handlers=[logging.StreamHandler(sys.stdout)],
+    format='[%(levelname)s] %(message)s',
+)
+for name in ['boto3', 's3transfer', 'botocore', 'boto', 'urllib3']:
+    logging.getLogger(name).setLevel(logging.ERROR)
