aeeeae14228fd8a0eb4dd72833be95513119efe9
Sofia Sazonova <sazonova@amazon.co.uk>
2025-07-01 09:55:30 +0100

    Remove unused logging + Unknown name

--- backend/dataall/modules/metadata_forms/handlers/metadata_form_handler.py
+++ backend/dataall/modules/metadata_forms/handlers/metadata_form_handler.py
@@ -1,10 +1,6 @@
-import logging
-
 from dataall.core.tasks.service_handlers import Worker
 from dataall.core.tasks.db.task_models import Task
 from dataall.modules.metadata_forms.services.metadata_form_enforcement_service import MetadataFormEnforcementService
-
-log = logging.getLogger(__name__)
 
 
 class EcsMetadataFormHandler:
@@ -13,5 +9,5 @@ class EcsMetadataFormHandler:
     def notify_owners_of_enforcement(engine, task: Task):
         with engine.scoped_session() as session:
             MetadataFormEnforcementService.notify_owners_of_enforcement(
-                session=session, rule_uri=task.targetUri, mf_name=task.payload.get('mf_name')
+                session=session, rule_uri=task.targetUri, mf_name=task.payload.get('mf_name', 'Unknown name')
             )
